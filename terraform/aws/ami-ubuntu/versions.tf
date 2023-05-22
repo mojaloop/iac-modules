84c3c3ae50937a098f4e4761e3b11ff0a470603c
David Fry <david.fry@modusbox.com>
2023-05-23 00:50:20 +0100

    downgrade

--- terraform/aws/ami-ubuntu/versions.tf
+++ terraform/aws/ami-ubuntu/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0"
   required_providers {
-    aws = "~> 4.67"
+    aws = "~> 3.74"
   }
 }
--- terraform/aws/post-config-k8s/module_providers.tf
+++ terraform/aws/post-config-k8s/module_providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
   
   required_providers {
-    aws   = "~> 4.67"
+    aws   = "~> 3.74"
     gitlab = {
       source = "gitlabhq/gitlab"
       version = "~> 16.0"
