9239b21f15dcea40919371e47efc460e1f1c8d97
David Fry <david.fry@modusbox.com>
2023-06-09 20:35:04 +0100

    rm circular dependency

--- terraform/vault/control-center-vault-config/vault.tf
+++ terraform/vault/control-center-vault-config/vault.tf
@@ -6,12 +6,12 @@ resource "vault_jwt_auth_backend" "gitlab_oidc" {
   oidc_client_id      = var.vault_oauth_app_client_id
   oidc_client_secret  = var.vault_oauth_app_client_secret
   bound_issuer        = "https://${var.gitlab_hostname}"
-  default_role        = vault_jwt_auth_backend_role.techops_admin_oidc.role_name
+  default_role        = var.gitlab_admin_rbac_group
 }
 
 resource "vault_jwt_auth_backend_role" "techops_admin_oidc" {
   backend         = vault_jwt_auth_backend.gitlab_oidc.path
-  role_name       = "techops-admin"
+  role_name       = var.gitlab_admin_rbac_group
   token_policies  = [vault_policy.admin.name]
   bound_audiences = [var.vault_oauth_app_client_id]
   oidc_scopes     = ["openid"]
