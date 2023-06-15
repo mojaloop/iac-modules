abecef0ab3eab53bd9f43218cd664396ccd6c174
David Fry <david.fry@modusbox.com>
2023-06-15 23:08:32 +0100

    rm count 5

--- terraform/aws/control-center-infra/loadbalancer.tf
+++ terraform/aws/control-center-infra/loadbalancer.tf
@@ -55,10 +55,9 @@ resource "aws_acm_certificate" "wildcard_cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  count = 5
-  name            = aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_name[count.index]
-  records         = [aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_value[count.index]]
-  type            = aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_type[count.index]
+  name            = aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_name[0]
+  records         = [aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_value[0]]
+  type            = aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_type[0]
   ttl             = 60
   allow_overwrite = true
   zone_id  = module.base_infra.public_zone.id
