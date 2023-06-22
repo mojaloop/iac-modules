9a05f6eaa08100640666f1d1ac8b343b056ab2bd
David Fry <david.fry@modusbox.com>
2023-06-22 19:36:04 +0100

    clean up script

--- terraform/control-center/init/deleteAll.sh
+++ terraform/control-center/init/deleteAll.sh
@@ -1,10 +1,6 @@
-export IAC_TERRAFORM_MODULES_TAG=v0.9.79
-export IAC_TEMPLATES_TAG=$IAC_TERRAFORM_MODULES_TAG
-export CONTROL_CENTER_CLOUD_PROVIDER=aws
-export ANSIBLE_BASE_OUTPUT_DIR=/tmp/output
-export PRIVATE_REPO_TOKEN=nullvalue
-export PRIVATE_REPO_USER=nullvalue
-export PRIVATE_REPO=example.com
-export AWS_PROFILE=oss
-
+cd /iac-run-dir
+source setenv
+cd -
+source setlocalenv.sh
+cd control-center-deploy
 terragrunt run-all destroy --terragrunt-non-interactive
