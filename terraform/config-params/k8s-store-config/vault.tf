1854e95a85790d7d893cac9c1100f195007edac0
David Fry <david.fry@modusbox.com>
2023-06-30 18:41:10 +0100

    get ride of vault mount

--- terraform/config-params/k8s-store-config/vault.tf
+++ terraform/config-params/k8s-store-config/vault.tf
@@ -1,15 +1,6 @@
-resource "vault_mount" "kv_secret" {
-  path                      = var.kv_path
-  type                      = "kv-v2"
-  options                   = { version = "2" }
-  default_lease_ttl_seconds = "120"
-}
-
-
-resource "vault_kv_secret_v2" "secrets_var_map" {
+resource "vault_generic_secret" "secrets_var_map" {
   for_each            = var.secrets_key_map
-  mount               = vault_mount.kv_secret.path
-  name                = "${var.cluster_name}/${each.value}"
+  path                = "${vault_mount.kv_secret.path}/${var.cluster_name}/${each.value}"
   delete_all_versions = true
   data_json = jsonencode(
     {
--- terraform/gitops-base-config/stored-params.tf
+++ terraform/gitops-base-config/stored-params.tf
@@ -18,19 +18,12 @@ data "gitlab_project_variable" "longhorn_backups_bucket_name" {
   key     = var.properties_key_map["longhorn_backups_bucket_name_key"]
 }
 
-resource "vault_mount" "kv_secret" {
-  path                      = var.kv_path
-  type                      = "kv-v2"
-  options                   = { version = "2" }
-  default_lease_ttl_seconds = "120"
-}
 # need to get these by hand because loki doesnt support k8s secret env vars.
-data "vault_kv_secret_v2" "grafana_oauth_client_id" {
-  mount = vault_mount.kv_secret
-  name = "${var.cluster_name}/${var.grafana_oidc_client_id_secret_key}"
+
+data "vault_generic_secret" "grafana_oauth_client_id" {
+  path = "${var.kv_path}/${var.cluster_name}/${var.grafana_oidc_client_id_secret_key}"
 }
 
-data "vault_kv_secret_v2" "grafana_oauth_client_secret" {
-  mount = vault_mount.kv_secret
-  name = "${var.cluster_name}/${var.grafana_oidc_client_secret_secret_key}"
+data "vault_generic_secret" "grafana_oauth_client_secret" {
+  path = "${var.kv_path}/${var.cluster_name}/${var.grafana_oidc_client_secret_secret_key}"
 }
