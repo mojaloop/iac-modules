37e171825956d7de06a9312a30370f344e170035
David Fry <david.fry@modusbox.com>
2023-06-30 18:48:16 +0100

    fix path

--- terraform/config-params/k8s-store-config/vault.tf
+++ terraform/config-params/k8s-store-config/vault.tf
@@ -1,6 +1,6 @@
 resource "vault_generic_secret" "secrets_var_map" {
   for_each            = var.secrets_key_map
-  path                = "${vault_mount.kv_secret.path}/${var.cluster_name}/${each.value}"
+  path                = "${var.kv_path}/${var.cluster_name}/${each.value}"
   delete_all_versions = true
   data_json = jsonencode(
     {
