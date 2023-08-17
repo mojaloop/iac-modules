867ad6309abebfef38c8c4ae9541560c1e1db2b5
David Fry <david.fry@modusbox.com>
2023-08-17 11:55:13 +0100

    fix json encoding issues

--- terraform/aws/support-svcs/deploy-rds/outputs.tf
+++ terraform/aws/support-svcs/deploy-rds/outputs.tf
@@ -32,7 +32,7 @@ output "secrets_var_map" {
   sensitive = true
   value = {
     for index, rds_module in module.rds : 
-      var.rds_services[index].external_resource_config.password_key_name => data.aws_secretsmanager_secret_version.rds_passwords[index].secret_string
+      var.rds_services[index].external_resource_config.password_key_name => jsondecode(data.aws_secretsmanager_secret_version.rds_passwords[index].secret_string)["password"]
   }
 }
 
