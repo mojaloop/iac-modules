baeb9f8ed54a66b3295d726694c7d4d35c52b7f5
David Fry <david.fry@modusbox.com>
2023-09-26 17:55:39 +0100

    cleanup output

--- terraform/aws/eks/outputs.tf
+++ terraform/aws/eks/outputs.tf
@@ -121,7 +121,6 @@ output "bastion_hosts_var_maps" {
   value = {
     ansible_ssh_common_args = "-o StrictHostKeyChecking=no"
     egress_gateway_cidr     = var.vpc_cidr
-    kubeconfig_content      = module.eks_kubeconfig.kubeconfig
   }
 }
 
