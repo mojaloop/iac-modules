4e5f058c5500fa5c1f1ca6fd7775da47c28b2c64
David Fry <david.fry@modusbox.com>
2023-09-29 22:37:09 +0100

    add k8s provider

--- terraform/aws/eks/infra.tf
+++ terraform/aws/eks/infra.tf
@@ -150,3 +150,15 @@ locals {
   master_security_groups = var.master_node_supports_traffic ? concat(local.base_security_groups, local.traffic_security_groups) : local.base_security_groups
   agent_security_groups  = concat(local.base_security_groups, local.traffic_security_groups)
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
