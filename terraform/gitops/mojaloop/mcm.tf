2820d4161723eb4c910210b42893c8bfc9ae67c8
David Fry <david.fry@modusbox.com>
2023-10-05 18:10:19 +0100

    missed var

--- terraform/gitops/mojaloop/mcm.tf
+++ terraform/gitops/mojaloop/mcm.tf
@@ -67,6 +67,7 @@ module "generate_mcm_files" {
     internal_load_balancer_dns                    = var.internal_load_balancer_dns
     external_load_balancer_dns                    = var.external_load_balancer_dns
     istio_internal_gateway_name                   = var.istio_internal_gateway_name
+    int_interop_switch_fqdn                       = var.internal_interop_switch_fqdn
   }
   file_list       = ["values-mcm.yaml", "kustomization.yaml", "vault-rbac.yaml", "vault-secret.yaml", "vault-agent.yaml", "configmaps/vault-config-configmap.hcl", "configmaps/vault-config-init-configmap.hcl", "istio-gateway.yaml", "vault-certificate.yaml"]
   template_path   = "${path.module}/../generate-files/templates/mcm"
