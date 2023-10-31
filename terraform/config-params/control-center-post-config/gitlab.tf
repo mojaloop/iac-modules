4509a2e4832b8282b1600649e7037f09b51cfdc3
David Fry <david.fry@modusbox.com>
2023-10-31 14:46:52 +0000

    add managed_svc_cloud_platform

--- terraform/config-params/control-center-post-config/gitlab.tf
+++ terraform/config-params/control-center-post-config/gitlab.tf
@@ -43,6 +43,15 @@ resource "gitlab_project_variable" "cloud_platform" {
   masked    = false
 }
 
+resource "gitlab_project_variable" "managed_svc_cloud_platform" {
+  for_each  = var.env_map
+  project   = gitlab_project.envs[each.key].id
+  key       = "MANAGED_SVC_CLOUD_PLATFORM"
+  value     = each.value["managed_svc_cloud_platform"]
+  protected = false
+  masked    = false
+}
+
 resource "gitlab_project_variable" "cloud_platform_client_secret_name" {
   for_each  = var.env_map
   project   = gitlab_project.envs[each.key].id
