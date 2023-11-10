8afc247eef6cc9b413fbc494a7c9c4692d50be9c
Oscar R Coar <oscar.cobar@portx.io>
2023-11-09 19:04:52 -0600

    initial commit provision MKS (iprod-262)

--- terraform/aws/support-svcs/deploy-kafka/infra.tf
+++ terraform/aws/support-svcs/deploy-kafka/infra.tf
@@ -1,93 +1,23 @@
-## Database modules
-
-/*
-resource "aws_kms_key" "managed_db_key" {
-  description             = "KMS Key used to manage passwords for managed dbs"
-  deletion_window_in_days = 10
-}
-*/
-
-module "msk_kafka_cluster" {
-  for_each   = var.kafka_services
-  source     = "terraform-aws-modules/msk-kafka-cluster/aws"
-
-  identifier = each.key
-
-  engine              = each.value.external_resource_config.engine
-  engine_version      = each.value.external_resource_config.engine_version
-  instance_class      = each.value.external_resource_config.instance_class
-  allocated_storage   = each.value.external_resource_config.allocated_storage
-  storage_encrypted   = each.value.external_resource_config.storage_encrypted
-  multi_az            = each.value.external_resource_config.multi_az
-  skip_final_snapshot = each.value.external_resource_config.skip_final_snapshot
-
-  db_name  = each.value.external_resource_config.db_name
-  username = each.value.external_resource_config.username
-  port     = each.value.external_resource_config.port
-  master_user_secret_kms_key_id = aws_kms_key.managed_db_key.key_id
-  vpc_security_group_ids = [var.security_group_id]
-
-  maintenance_window = each.value.external_resource_config.maintenance_window
-  backup_window      = each.value.external_resource_config.backup_window
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = each.value.external_resource_config.monitoring_interval
-  monitoring_role_name   = "${each.value.external_resource_config.db_name}-RDSMonitoringRole"
-  create_monitoring_role = true
-
-  tags = each.value.external_resource_config.tags
-
-  # DB subnet group
-  create_db_subnet_group = true
-  subnet_ids             = var.private_subnets
-
-  # DB parameter group
-  family = each.value.external_resource_config.family
-
-  # DB option group
-  major_engine_version = each.value.external_resource_config.major_engine_version
-
-  # Database Deletion Protection
-  deletion_protection = each.value.external_resource_config.deletion_protection
-
-  parameters = each.value.external_resource_config.parameters
-
-  options = each.value.external_resource_config.options
-}
-
-data "aws_secretsmanager_secret" "rds_passwords" {
-  for_each = module.rds
-  arn = each.value.db_instance_master_user_secret_arn
-}
-
-data "aws_secretsmanager_secret_version" "rds_passwords" {
-  for_each = data.aws_secretsmanager_secret.rds_passwords
-  secret_id     = each.value.id
-}
-
-
-
 module "msk_kafka_cluster" {
   source = "terraform-aws-modules/msk-kafka-cluster/aws"
 
-  name                   = local.name
-  kafka_version          = "3.4.0"
-  number_of_broker_nodes = 3
-  enhanced_monitoring    = "PER_TOPIC_PER_PARTITION"
+  name                   = each.value.external_resource_config.name
+  kafka_version          = each.value.external_resource_config.kafka_version
+  number_of_broker_nodes = each.value.external_resource_config.number_of_broker_nodes
+  enhanced_monitoring    = each.value.external_resource_config.enhanced_monitoring
 
   broker_node_client_subnets = ["subnet-12345678", "subnet-024681012", "subnet-87654321"]
   broker_node_storage_info = {
     ebs_storage_info = { volume_size = 100 }
   }
-  broker_node_instance_type   = "kafka.t3.small"
+  broker_node_instance_type   = each.value.external_resource_config.broker_node_instance_type
   broker_node_security_groups = ["sg-12345678"]
 
-  encryption_in_transit_client_broker = "TLS"
-  encryption_in_transit_in_cluster    = true
+  encryption_in_transit_client_broker = each.value.external_resource_config.encryption_in_transit_client_broker
+  encryption_in_transit_in_cluster    = each.value.external_resource_config.encryption_in_transit_in_cluster
 
-  configuration_name        = "example-configuration"
-  configuration_description = "Example configuration"
+  configuration_name        = each.value.external_resource_config.configuration_name
+  configuration_description = each.value.external_resource_config.configuration_description
   configuration_server_properties = {
     "auto.create.topics.enable" = true
     "delete.topic.enable"       = true
