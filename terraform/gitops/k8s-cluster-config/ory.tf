85f7c5d07c3a35970512fad5fdfd3500a2138616
David Fry <david.fry@modusbox.com>
2024-01-20 22:48:18 +0000

    fix kc host refs

--- terraform/gitops/k8s-cluster-config/ory.tf
+++ terraform/gitops/k8s-cluster-config/ory.tf
@@ -28,6 +28,7 @@ module "generate_ory_files" {
     kratos_oidc_client_secret_secret_path = local.keycloak_secrets_path
     keycloak_kratos_realm_name            = var.keycloak_kratos_realm_name
     keycloak_name                         = var.keycloak_name
+    keycloak_fqdn                         = local.keycloak_fqdn
     keto_dsn_secretname                   = "keto_db_dsn_secret"
     kratos_dsn_secretname                 = "kratos_db_dsn_secret"
     istio_external_gateway_namespace      = var.istio_external_gateway_namespace
