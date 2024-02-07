d80ea33425b2c3647d16d916a7db8ac9449a2ad1
geka.evk <geka.evk@gmail.com>
2024-02-07 09:20:12 +0000

    feat-iprod-386: added ory-rbac-crd.tf

---
+++ terraform/gitops/mojaloop/ory-rbac-crd.tf
@@ -0,0 +1,83 @@
+locals {
+  apiResourcesFile = yamldecode(
+    file("${var.rbac_custom_resources_dir}/mojaloop-rbac-api-resources.yaml")
+  )
+  rolesPermissionsFile = yamldecode(
+    file("${var.rbac_custom_resources_dir}/mojaloop-rbac-permissions.yaml")
+  )
+  mojaloopRoles = local.rolesPermissionsFile["roles"]
+  permissionExclusions = local.rolesPermissionsFile["permission-exclusions"]
+  oathkeeperMutators = [
+    { handler = "header" }
+  ]
+}
+
+resource "kubernetes_manifest" "bof-roles" {
+  for_each = {for role in local.mojaloopRoles: role.rolename => role}
+  manifest = {
+    apiVersion = "mojaloop.io/v1"
+    kind       = "MojaloopRole"
+
+    metadata = {
+      name = lower(replace(each.value.rolename, "_", "-"))
+      namespace = "mojaloop"
+    }
+
+    spec = {
+      role = each.value.rolename
+      permissions = each.value.permissions
+    }
+  }
+  provider = kubernetes.k8s-main
+}
+
+resource "kubernetes_manifest" "bof-permission-exclusions" {
+  for_each = {for pe in local.permissionExclusions: pe.name => pe}
+  manifest = {
+    apiVersion = "mojaloop.io/v1"
+    kind       = "MojaloopPermissionExclusions"
+
+    metadata = {
+      name = lower(replace(each.value.name, "_", "-"))
+      namespace = "mojaloop"
+    }
+
+    spec = {
+      permissionsA = each.value.permissionsA
+      permissionsB = each.value.permissionsB
+    }
+  }
+  provider = kubernetes.k8s-main
+}
+
+resource "kubernetes_manifest" "bof-api-resources" {
+  for_each = {for ar in local.apiResourcesFile: ar.name => ar}
+  manifest = {
+    apiVersion = "oathkeeper.ory.sh/v1alpha1"
+    kind       = "Rule"
+
+    metadata = {
+      name = lower(replace(each.value.name, "_", "-"))
+      namespace = "mojaloop"
+    }
+
+    spec = {
+      match = each.value.match
+      authenticators = each.value.authenticators
+      authorizer_permission = each.value.authorizer_permission
+      mutators = local.oathkeeperMutators
+#      authorizer:
+#        handler: remote_json
+#        config:
+#            remote: http://keto-read/check
+#            payload: |
+#                {
+#                "namespace": "permission",
+#                "object": "settlementView",
+#                "relation": "granted",
+#                "subject": "{{ printf "{{ print .Subject }}" }}"
+#                }
+    }
+  }
+  provider = kubernetes.k8s-main
+}
--- terraform/gitops/mojaloop/variables.tf
+++ terraform/gitops/mojaloop/variables.tf
@@ -166,3 +166,9 @@ variable "cert_manager_namespace" {
   type = string
   description = "cert_manager_namespace"
 }
+
+variable "rbac_custom_resources_dir" {
+  type        = string
+  description = "folder with RBAC permissions and API resources files"
+  default     = "../../k8s"
+}
