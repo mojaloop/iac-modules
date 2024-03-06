783d43a3ab4ad7d0eb16a19e194762a933f23e4a
Sijo George <sijogeorge5722@gmail.com>
2024-03-06 19:24:13 +0530

    Fixing refresh templates

--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/refresh-repo.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/refresh-repo.sh
@@ -6,9 +6,11 @@ WORKING_DIR=$PWD
 BASE_GITLAB_URL=https://root:${ROOT_TOKEN}@${CI_SERVER_HOST}/iac
 IAC_MODULES_TAG=$5
 
+mkdir -p $TMP_GIT_REPO
+git clone ${TEMPLATE_REPO_URL} $TMP_GIT_REPO
 cd $TMP_GIT_REPO && git checkout ${IAC_MODULES_TAG}
-mkdir -p $TMP_TEMPLATES_DIR/${CURRENT_ENV_NAME}
-cp -r ${CI_TEMPLATE_PATH}/. ${K8S_TEMPLATE_PATH}/. $TMP_TEMPLATES_DIR/${CURRENT_ENV_NAME}
+mkdir -p $TMP_TEMPLATE_DIR/${CURRENT_ENV_NAME}
+cp -r ${CI_TEMPLATE_PATH}/. ${K8S_TEMPLATE_PATH}/. $TMP_TEMPLATE_DIR/${CURRENT_ENV_NAME}
 TMP_REPO_DIR=/tmp/gitclone${CURRENT_ENV_NAME}
 mkdir -p $TMP_REPO_DIR
 git clone ${BASE_GITLAB_URL}/${CURRENT_ENV_NAME} $TMP_REPO_DIR
@@ -18,6 +20,6 @@ cp $WORKING_DIR/environment.yaml .
 git config --global user.email "root@${gitlab_hostname}"
 git config --global user.name "root"
 git add .
-git commit -m "refreshing templates from release ${versionarray[$i]} to project"
+git commit -m "refreshing templates from release ${IAC_MODULES_TAG} to project"
 git push
 rm -rf $TMP_REPO_DIR
