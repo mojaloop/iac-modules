ee9c14eb4efef31c5c6c8ac1b568956b1055d945
David Fry <david.fry@modusbox.com>
2024-03-08 11:58:01 +0000

    cleanup destroy vars script

--- terraform/gitlab/ci-templates/k8s-cluster/set-ansible-destroy-preq-vars.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/set-ansible-destroy-preq-vars.sh
@@ -1,7 +1,4 @@
-
-export d_ansible_collection_tag=$(cat environment.json |  jq -r ".envs" | jq -r '.[] | select(.env == '\"$CI_PROJECT_NAME\"').ansible_collection_tag')
-export d_cluster_type=$(cat environment.json |  jq -r ".envs" | jq -r '.[] | select(.env == '\"$CI_PROJECT_NAME\"').k8s_cluster_type')
-export destroy_ansible_playbook="mojaloop.iac.argo${d_cluster_type}_cluster_destroy"
+export destroy_ansible_playbook="mojaloop.iac.argo${k8s_cluster_type}_cluster_destroy"
 export d_ansible_collection_url="git+https://github.com/mojaloop/iac-ansible-collection-roles.git#/mojaloop/iac"
 export destroy_ansible_inventory="$ANSIBLE_BASE_OUTPUT_DIR/k8s-deploy/inventory"
-export destroy_ansible_collection_complete_url=$d_ansible_collection_url,$d_ansible_collection_tag
+export destroy_ansible_collection_complete_url=$d_ansible_collection_url,$ansible_collection_tag
