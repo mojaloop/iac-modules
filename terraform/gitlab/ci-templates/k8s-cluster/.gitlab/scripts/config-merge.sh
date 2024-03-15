2176d7b892811e3b345c5c75cd3c3ca6d5e64a10
Sijo George <sijogeorge5722@gmail.com>
2024-03-15 17:15:01 +0530

    Including list of dicts for config merge

--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/config-merge.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/config-merge.sh
@@ -1,17 +1,17 @@
 mkdir -p $CONFIG_PATH
-for configFile in {'aws-vars.yaml','cluster-config.yaml','common-vars.yaml','mojaloop-rbac-permissions.yaml','mojaloop-vars.yaml','pm4ml-vars.yaml','bare-metal-vars.yaml','pm4ml-rbac-permissions.yaml'};
+for configFile in {'aws-vars.yaml','cluster-config.yaml','common-vars.yaml','mojaloop-rbac-permissions.yaml','mojaloop-vars.yaml','pm4ml-vars.yaml','bare-metal-vars.yaml','pm4ml-rbac-permissions.yaml','mojaloop-stateful-resources.json','common-stateful-resources.json','mojaloop-rbac-api-resources.yaml'};
 do
     echo $configFile
     python3 .gitlab/scripts/dictmerge.py default-config/$configFile custom-config/$configFile $CONFIG_PATH;
 done;
 
-for configFile in {'mojaloop-stateful-resources.json','common-stateful-resources.json','mojaloop-rbac-api-resources.yaml'};
-do
-    if [ -f custom-config/$configFile ]; then
-            cp custom-config/$configFile $CONFIG_PATH
-            echo "custom-config/$configFile copied to $CONFIG_PATH"
-    else
-            cp default-config/$configFile $CONFIG_PATH
-            echo "default-config/$configFile copied to $CONFIG_PATH"
-     fi;
-done;
+# for configFile in {'mojaloop-stateful-resources.json','common-stateful-resources.json','mojaloop-rbac-api-resources.yaml'};
+# do
+#     if [ -f custom-config/$configFile ]; then
+#             cp custom-config/$configFile $CONFIG_PATH
+#             echo "custom-config/$configFile copied to $CONFIG_PATH"
+#     else
+#             cp default-config/$configFile $CONFIG_PATH
+#             echo "default-config/$configFile copied to $CONFIG_PATH"
+#      fi;
+# done;
--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/dictmerge.py
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/dictmerge.py
@@ -3,6 +3,16 @@ import json
 import sys
 import os
 
+#def str_presenter(dumper, data):
+#    """configures yaml for dumping multiline strings
+#    Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data"""
+#    if len(data.splitlines()) > 1:  # check for multiline string
+#        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+#    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+#yaml.add_representer(str, str_presenter)
+#yaml.representer.SafeRepresenter.add_representer(str, str_presenter) # to use with safe_dum
+
 yaml.Dumper.ignore_aliases = lambda *args : True
 
 def mergedicts(dict1, dict2):
@@ -18,6 +28,34 @@ def mergedicts(dict1, dict2):
             yield (k, dict1[k])
         else:
             yield (k, dict2[k])
+
+def writeDict(mergedItems, fileType, outputFilename):
+    if fileType == ".json":
+        with open(outputFilename, 'w') as file:
+            json.dump(mergedItems, file, indent=4)
+    elif fileType == ".yaml":
+        with open(outputFilename, 'w') as file:    
+            yaml.dump(mergedItems, file, indent=4,default_flow_style=False)    
+
+
+def mergeListOfDicts(data1, data2, fileName, outputFilename, fileType):
+    mergedItems=[]
+    
+    if len(data2) == 0:
+        print("The custom-config file ",fileName, "is empty ,so using the default configuration file")
+        mergedItems = data1
+        writeDict(mergedItems, fileType, outputFilename)
+        exit(1)     
+    
+    if len(data1) != len(data2):
+        print("Number of elements in custom-config and default_config differs for",fileName, ",so using the default configuration file")
+        mergedItems = data1
+        writeDict(mergedItems, fileType, outputFilename)
+        exit(1)
+    
+    for item1, item2 in zip(data1, data2):
+        mergedItems.append(dict(mergedicts(item1, item2)))
+    writeDict(mergedItems, fileType, outputFilename)
 
 if len(sys.argv) == 4:
     default_config_file = sys.argv[1]
@@ -53,8 +91,12 @@ if os.path.isfile(custom_config_file):
         with open(custom_config_file, 'r') as f:
             data2 = yaml.load(f, Loader=yaml.SafeLoader) or {}
     elif customExt == ".json":
-        with open(custom_config_file, 'r') as f:
-            data2 = json.load(f)    
+        try:
+            with open(custom_config_file, 'r') as f:
+                data2 = json.load(f)    
+        except json.JSONDecodeError:
+            print("Could not parse the custom config file", custom_config_file," so assigning empty dict")
+            data2 = {}
 
     else:
        print("File type not supported")
@@ -68,7 +110,7 @@ if fileName == "pm4ml-vars.yaml":
     mergedItems=[]
     mergedDict = {}
     if data2 == {} or len(data2["pm4mls"]) == 0:
-        print("custom-config/pm4ml-vars.yaml is empty, please provide the correct configuration")
+        print("custom-config/pm4ml-vars.yaml is empty, please provide the correct configuration in case if its a payment manager deployment")
         with open(outputFilename, 'w') as file:
             yaml.dump(mergedDict, file, indent=4 , default_flow_style=False)
         exit(1)
@@ -78,25 +120,8 @@ if fileName == "pm4ml-vars.yaml":
     with open(outputFilename, 'w') as file:
         yaml.dump(mergedDict, file, indent=4 , default_flow_style=False)
 
-elif fileName in ( "common-stateful-resources.json" , "mojaloop-stateful-resources.json" ):
-    mergedItems=[]
-    if len(data1) != len(data2):
-        print("Number of elements in custom-config and default_config differs for",fileName, ",please correct the config file")
-        exit(1)
-    for item1, item2 in zip(data1, data2):
-        mergedItems.append(dict(mergedicts(item1, item2)))
-    with open(outputFilename, 'w') as file:
-        json.dump(mergedItems, file, indent=4)
-
-elif fileName in ( "mojaloop-rbac-api-resources.yaml" ):
-    mergedItems=[]
-    if len(data1) != len(data2):
-        print("Number of elements in custom-config and default_config differs for",fileName, ",please correct the config file")
-        exit(1)
-    for item1, item2 in zip(data1, data2):
-        mergedItems.append(dict(mergedicts(item1, item2)))
-    with open(outputFilename, 'w') as file:
-        yaml.dump(mergedItems, file, indent=4)
+elif fileName in ( "common-stateful-resources.json" , "mojaloop-stateful-resources.json" , "mojaloop-rbac-api-resources.yaml" ):
+    mergeListOfDicts(data1, data2, fileName, outputFilename, defaultExt)
 
 elif defaultExt == ".yaml":
     #result = yaml.dump(dict(mergedicts(data1, data2)), indent=4, sort_keys=True)
