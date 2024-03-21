5949baad08db7a1c742d873e74064c95d2791c6b
Sijo George <sijogeorge5722@gmail.com>
2024-03-21 16:51:36 +0530

    Adding refresh remplates for bootstrap

---
+++ terraform/gitlab/ci-templates/bootstrap/.gitlab/scripts/refresh-templates.sh
@@ -0,0 +1,31 @@
+CC_TMP_GIT_REPO=$1
+CC_TMP_TEMPLATE_DIR=$2
+ROOT_TOKEN=$3
+WORKING_DIR=$PWD
+BASE_GITLAB_URL=https://root:${ROOT_TOKEN}@${CI_SERVER_HOST}/iac
+IAC_MODULES_TAG=$4
+
+
+rm -rf $CC_TMP_GIT_REPO
+mkdir -p $CC_TMP_GIT_REPO
+git clone ${TEMPLATE_REPO_URL} $CC_TMP_GIT_REPO
+cd $CC_TMP_GIT_REPO && git checkout ${IAC_MODULES_TAG}
+
+rm -rf $CC_TMP_TEMPLATE_DIR
+mkdir -p $CC_TMP_TEMPLATE_DIR
+cp -r ${CC_CI_TEMPLATE_PATH}/. ${CC_TEMPLATE_PATH}/. $CC_TMP_TEMPLATE_DIR/
+
+
+CC_TMP_REPO_DIR=/tmp/gitclone-bootstrap
+mkdir -p $CC_TMP_REPO_DIR
+git clone ${BASE_GITLAB_URL}/bootstrap $CC_TMP_REPO_DIR
+cd $CC_TMP_REPO_DIR
+
+
+cp -r $CC_TMP_TEMPLATE_DIR/control-center-deploy/ $CC_TMP_TEMPLATE_DIR/control-center-pre-config/ $CC_TMP_TEMPLATE_DIR/control-center-post-config/ $CC_TMP_TEMPLATE_DIR/ansible-cc-deploy/ $CC_TMP_TEMPLATE_DIR/ansible-cc-post-deploy/  $CC_TMP_TEMPLATE_DIR/.gitlab  $CC_TMP_TEMPLATE_DIR/.gitlab-ci.yml  .
+
+git config --global user.email "root@${gitlab_hostname}"
+git config --global user.name "root"
+git add .
+git commit -m "refreshing templates from release ${IAC_MODULES_TAG} to project"
+git push
