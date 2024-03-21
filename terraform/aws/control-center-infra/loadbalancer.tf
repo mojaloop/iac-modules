e3af789d490891c81071ce8d999ae38753868f17
David Fry <david.fry@modusbox.com>
2024-03-21 20:40:31 +0000

    cleanup lb cert

--- terraform/aws/control-center-infra/loadbalancer.tf
+++ terraform/aws/control-center-infra/loadbalancer.tf
@@ -231,18 +231,26 @@ resource "aws_acm_certificate" "wildcard_cert" {
   }
 }
 
-# need to set count to 3 for the 3 records (domain name, wildcard and vault, if we add to SAN, need to increment count)
 resource "aws_route53_record" "cert_validation" {
-  count           = 7
-  name            = aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_name[count.index]
-  records         = [aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_value[count.index]]
-  type            = aws_acm_certificate.wildcard_cert.domain_validation_options.*.resource_record_type[count.index]
-  ttl             = 60
+  for_each = {
+    for dvo in aws_acm_certificate.wildcard_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+    # Skips the domain if it doesn't contain a wildcard
+    if length(regexall("\\*\\..+", dvo.domain_name)) > 0
+  }
+
   allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
   zone_id         = module.base_infra.public_zone.id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.wildcard_cert.arn
-  validation_record_fqdns = aws_route53_record.cert_validation[*].fqdn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
