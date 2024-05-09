a082b43ee65e1f180ee16f9304104d9b5b46044f
Sijo George <sijogeorge5722@gmail.com>
2024-05-09 21:07:54 +0530

    correcting the syntax

--- terraform/ansible/k8s-deploy/ansible.tf
+++ terraform/ansible/k8s-deploy/ansible.tf
@@ -91,7 +91,7 @@ locals {
   managed_stateful_resources         = { for managed_resource in local.enabled_stateful_resources : managed_resource.resource_name => managed_resource if managed_resource.external_service }
   
   external_stateful_resource_instance_addresses = { for address in data.gitlab_project_variable.external_stateful_resource_instance_address : address.key => address.value }
-  managed_svc_port_maps                         = { for service in local.managed_stateful_resources : [ 
+  managed_svc_port_maps                         = { for service in local.managed_stateful_resources : {
                                                    for logical_service_config in service : {
                                                      "${service.resource_name}" => {
                                                            "port"   = logical_service_config.logical_service_port
@@ -99,7 +99,7 @@ locals {
                                                            "dest"   = local.external_stateful_resource_instance_addresses[service.external_resource_config.instance_address_key_name]
                                                            }
                                                         }
-                                                     ]
+                                                     }
                                                    }    
 
 }
