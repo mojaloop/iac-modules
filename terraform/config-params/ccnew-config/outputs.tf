bb8cb1c470193a23b93bc153729c3df7807352b3
David Fry <david.fry@modusbox.com>
2024-06-13 18:57:52 +0100

    fix names

--- terraform/config-params/ccnew-config/outputs.tf
+++ terraform/config-params/ccnew-config/outputs.tf
@@ -1,15 +1,15 @@
 output "netbird_dashboard_client_id" {
-  value = zitadel_application_oidc.netbird_dashboard.netbird_dashboard_client_id
+  value = zitadel_application_oidc.netbird_dashboard.client_id
 }
 output "netbird_dashboard_client_secret" {
-  value     = zitadel_application_oidc.netbird_dashboard.netbird_dashboard_client_secret
+  value     = zitadel_application_oidc.netbird_dashboard.client_secret
   sensitive = true
 }
 output "netbird_cli_client_id" {
-  value = zitadel_application_oidc.netbird_cli.netbird_dashboard_client_id
+  value = zitadel_application_oidc.netbird_cli.client_id
 }
 output "netbird_cli_client_secret" {
-  value     = zitadel_application_oidc.netbird_cli.netbird_dashboard_client_secret
+  value     = zitadel_application_oidc.netbird_cli.client_secret
   sensitive = true
 }
 output "netbird_service_user_client_id" {
