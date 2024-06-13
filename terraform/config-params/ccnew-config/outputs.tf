a18ef4cdf577ecbda50873e892e87b99ca96648b
David Fry <david.fry@modusbox.com>
2024-06-13 19:00:46 +0100

    mark sensitive

--- terraform/config-params/ccnew-config/outputs.tf
+++ terraform/config-params/ccnew-config/outputs.tf
@@ -1,19 +1,22 @@
 output "netbird_dashboard_client_id" {
-  value = zitadel_application_oidc.netbird_dashboard.client_id
+  value     = zitadel_application_oidc.netbird_dashboard.client_id
+  sensitive = true
 }
 output "netbird_dashboard_client_secret" {
   value     = zitadel_application_oidc.netbird_dashboard.client_secret
   sensitive = true
 }
 output "netbird_cli_client_id" {
-  value = zitadel_application_oidc.netbird_cli.client_id
+  value     = zitadel_application_oidc.netbird_cli.client_id
+  sensitive = true
 }
 output "netbird_cli_client_secret" {
   value     = zitadel_application_oidc.netbird_cli.client_secret
   sensitive = true
 }
 output "netbird_service_user_client_id" {
-  value = zitadel_machine_user.service_user.client_id
+  value     = zitadel_machine_user.service_user.client_id
+  sensitive = true
 }
 output "netbird_service_user_client_secret" {
   value     = zitadel_machine_user.service_user.client_secret
