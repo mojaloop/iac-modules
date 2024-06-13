cb2c8d1f968d70c48bd9c821bbdf578edcd53e44
David Fry <david.fry@modusbox.com>
2024-06-13 15:03:59 +0100

    fix code

--- terraform/config-params/ccnew-config/main.tf
+++ terraform/config-params/ccnew-config/main.tf
@@ -2,6 +2,11 @@ data "zitadel_orgs" "default" {
   state = "ORG_STATE_ACTIVE"
 }
 
+data "zitadel_org" "default" {
+  for_each = toset(data.zitadel_orgs.default.ids)
+  id       = each.value
+}
+
 output "org_names" {
   value = toset([
     for org in data.zitadel_org.default : org.name
