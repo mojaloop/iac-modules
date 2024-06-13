c3c02d1d2a671b6bad9a05cfeb08e0984dc97de2
David Fry <david.fry@modusbox.com>
2024-06-13 15:25:47 +0100

    add root user

--- terraform/config-params/ccnew-config/main.tf
+++ terraform/config-params/ccnew-config/main.tf
@@ -7,6 +7,23 @@ data "zitadel_org" "default" {
   id       = each.value
 }
 
-output "org_id" {
-  value = [for org in data.zitadel_org.default : org.id if org.is_default][0]
+
+locals {
+  org_id = [for org in data.zitadel_org.default : org.id if org.is_default][0]
+}
+
+resource "zitadel_human_user" "default" {
+  org_id             = local.org_id
+  user_name          = "rootauto@zitadel.zitadel.cc22.ccnew.mojaloop.live"
+  first_name         = "root"
+  last_name          = "admin"
+  nick_name          = "admin"
+  display_name       = "admin"
+  preferred_language = "en"
+  gender             = "GENDER_MALE"
+  phone              = "+41799999999"
+  is_phone_verified  = true
+  email              = "test@zitadel.com"
+  is_email_verified  = true
+  initial_password   = "#Password1!"
 }
