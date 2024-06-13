24a4ac8006e865214b5cbb08bcf00dd3419146b9
David Fry <david.fry@modusbox.com>
2024-06-13 18:12:55 +0100

    typo

--- terraform/config-params/ccnew-config/outputs.tf
+++ terraform/config-params/ccnew-config/outputs.tf
@@ -8,7 +8,7 @@ output "netbird_dashboard_client_secret" {
 output "netbird_cli_client_id" {
   value = zitadel_application_oidc.netbird_cli.netbird_dashboard_client_id
 }
-output "netbird_dashboard_client_secret" {
+output "netbird_cli_client_secret" {
   value     = zitadel_application_oidc.netbird_cli.netbird_dashboard_client_secret
   sensitive = true
 }
