11b99c231c783d988c567c94757a58c601bf61ce
David Fry <david.fry@modusbox.com>
2024-06-14 13:26:39 +0100

    rm unused outputs

--- terraform/config-params/ccnew-config/outputs.tf
+++ terraform/config-params/ccnew-config/outputs.tf
@@ -2,16 +2,8 @@ output "netbird_dashboard_client_id" {
   value     = zitadel_application_oidc.netbird_dashboard.client_id
   sensitive = true
 }
-output "netbird_dashboard_client_secret" {
-  value     = zitadel_application_oidc.netbird_dashboard.client_secret
-  sensitive = true
-}
 output "netbird_cli_client_id" {
   value     = zitadel_application_oidc.netbird_cli.client_id
-  sensitive = true
-}
-output "netbird_cli_client_secret" {
-  value     = zitadel_application_oidc.netbird_cli.client_secret
   sensitive = true
 }
 output "netbird_service_user_client_id" {
