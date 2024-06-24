533774f307c27869ad2a28de89afac3541605be2
Sijo George <sijogeorge5722@gmail.com>
2024-06-24 19:11:33 +0530

    Adding ansible preq for local run

--- terraform/control-center/init/setlocalenv.sh
+++ terraform/control-center/init/setlocalenv.sh
@@ -1,2 +1,6 @@
 export IAC_TEMPLATES_TAG=$IAC_TERRAFORM_MODULES_TAG
 export CONTROL_CENTER_CLOUD_PROVIDER=aws
+export destroy_ansible_playbook="mojaloop.iac.control_center_post_destroy"
+export d_ansible_collection_url="git+https://github.com/mojaloop/iac-ansible-collection-roles.git#/mojaloop/iac"
+export destroy_ansible_inventory="$ANSIBLE_BASE_OUTPUT_DIR/control-center-post-config/inventory"
+export destroy_ansible_collection_complete_url=$d_ansible_collection_url,$ansible_collection_tag
