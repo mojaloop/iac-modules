8acbd79a5985a790413e8f3620e536bf14629e7d
Sijo George <sijogeorge5722@gmail.com>
2024-07-17 14:46:22 +0530

    Wrapper changes

--- terraform/ccnew/wrapper.sh
+++ terraform/ccnew/wrapper.sh
@@ -1,9 +1,10 @@
-# git tag command to check if a tag exist
-source setlocalvars. sh
+source setlocalvars.sh
 
 git pull
-tagcheck=$(git tag -l | grep Siac_terraform_modules_tag)
-if [L "Stagcheck" == "Siac_terraform_modules_tag" ]]; then
+
+tagcheck=$(git tag -l | grep $iac_terraform_modules_tag)
+
+if [[ "$tagcheck" == "$iac_terraform_modules_tag" ]]; then
 echo "tag exist"
 else
 exit -1
