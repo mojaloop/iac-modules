b97ca9347b76482e0b48744314566a9f88222169
David Fry <david.fry@modusbox.com>
2024-07-20 10:14:58 +0100

    try and cleanup oidc config

--- terraform/config-params/ccnew-config/netbird/netbird.tf
+++ terraform/config-params/ccnew-config/netbird/netbird.tf
@@ -1,15 +1,16 @@
 resource "zitadel_project" "netbird" {
-  name   = "NETBIRD"
-  org_id = local.org_id
+  name                   = "Netbird"
+  org_id                 = local.org_id
+  project_role_assertion = true
 }
 
-resource "zitadel_application_oidc" "netbird_dashboard" {
+resource "zitadel_application_oidc" "netbird" {
   project_id                  = zitadel_project.netbird.id
   org_id                      = local.org_id
-  name                        = "Netbird-Dashboard"
-  redirect_uris               = ["https://${var.dashboard_fqdn}/nb-auth", "https://${var.dashboard_fqdn}/nb-silent-auth"]
+  name                        = "netbird"
+  redirect_uris               = ["https://${var.dashboard_fqdn}/nb-auth", "https://${var.dashboard_fqdn}/nb-silent-auth", "http://localhost:53000"]
   response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
-  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE", "OIDC_GRANT_TYPE_REFRESH_TOKEN"]
+  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE", "OIDC_GRANT_TYPE_REFRESH_TOKEN", "OIDC_GRANT_TYPE_DEVICE_CODE"]
   post_logout_redirect_uris   = ["https://${var.dashboard_fqdn}/"]
   app_type                    = "OIDC_APP_TYPE_USER_AGENT"
   auth_method_type            = "OIDC_AUTH_METHOD_TYPE_NONE"
@@ -19,27 +20,27 @@ resource "zitadel_application_oidc" "netbird_dashboard" {
   access_token_role_assertion = true
 }
 
-resource "zitadel_application_oidc" "netbird_cli" {
-  project_id                  = zitadel_project.netbird.id
-  org_id                      = local.org_id
-  name                        = "Netbird-Cli"
-  redirect_uris               = ["http://localhost:53000/", "http://localhost:54000/"]
-  response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
-  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE", "OIDC_GRANT_TYPE_DEVICE_CODE", "OIDC_GRANT_TYPE_REFRESH_TOKEN"]
-  post_logout_redirect_uris   = ["http://localhost:53000/"]
-  app_type                    = "OIDC_APP_TYPE_USER_AGENT"
-  auth_method_type            = "OIDC_AUTH_METHOD_TYPE_NONE"
-  version                     = "OIDC_VERSION_1_0"
-  dev_mode                    = true
-  access_token_type           = "OIDC_TOKEN_TYPE_JWT"
-  access_token_role_assertion = true
-}
+# resource "zitadel_application_oidc" "netbird_cli" {
+#   project_id                  = zitadel_project.netbird.id
+#   org_id                      = local.org_id
+#   name                        = "Netbird-Cli"
+#   redirect_uris               = ["http://localhost:53000/", "http://localhost:54000/"]
+#   response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
+#   grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE", "OIDC_GRANT_TYPE_DEVICE_CODE", "OIDC_GRANT_TYPE_REFRESH_TOKEN"]
+#   post_logout_redirect_uris   = ["http://localhost:53000/"]
+#   app_type                    = "OIDC_APP_TYPE_USER_AGENT"
+#   auth_method_type            = "OIDC_AUTH_METHOD_TYPE_NONE"
+#   version                     = "OIDC_VERSION_1_0"
+#   dev_mode                    = true
+#   access_token_type           = "OIDC_TOKEN_TYPE_JWT"
+#   access_token_role_assertion = true
+# }
 
 
 resource "zitadel_machine_user" "service_user" {
   org_id            = local.org_id
-  user_name         = "netbird-service-account"
-  name              = "Netbird Service Account"
+  user_name         = "netbird"
+  name              = "netbird"
   description       = "Netbird Service Account for IDP management"
   with_secret       = true
   access_token_type = "ACCESS_TOKEN_TYPE_JWT"
--- terraform/config-params/ccnew-config/netbird/outputs.tf
+++ terraform/config-params/ccnew-config/netbird/outputs.tf
@@ -1,11 +1,8 @@
-output "netbird_dashboard_client_id" {
-  value     = zitadel_application_oidc.netbird_dashboard.client_id
+output "netbird_client_id" {
+  value     = zitadel_application_oidc.netbird.client_id
   sensitive = true
 }
-output "netbird_cli_client_id" {
-  value     = zitadel_application_oidc.netbird_cli.client_id
-  sensitive = true
-}
+
 output "netbird_service_user_client_id" {
   value     = zitadel_machine_user.service_user.client_id
   sensitive = true
