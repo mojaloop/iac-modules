850a488592c122031b7bde950e742a5f871120bc
Kalin Krustev <kalin.krustev@gmail.com>
2024-07-22 09:02:37 +0000

    tenancy vault

--- terraform/gitops/proxy-pm4ml/tenancy_vault.tf
+++ terraform/gitops/proxy-pm4ml/tenancy_vault.tf
@@ -1,9 +1,7 @@
 # Based on:
 # https://github.com/hashicorp/terraform-provider-vault/issues/1900
-# https://github.com/hashicorp/terraform-provider-vault/issues/1727
 resource "vault_kv_secret_backend_v2" "cas_required" {
   mount        = "/secret"
-  cas_required = true
 }
 
 # Create dummy secrets so that deployment can proceed
@@ -11,7 +9,6 @@ resource "vault_kv_secret_v2" "mcm_client_a" {
   for_each     = var.app_var_map
   mount        = vault_kv_secret_backend_v2.cas_required.mount
   name         = "${var.cluster_name}/${each.key}/mcm_client_secret_a"
-  options      = { cas = 0 }
   data_json    = jsonencode({ value = "dummy" })
   disable_read = true
   lifecycle {
@@ -24,7 +21,6 @@ resource "vault_kv_secret_v2" "mcm_client_b" {
   for_each     = var.app_var_map
   mount        = vault_kv_secret_backend_v2.cas_required.mount
   name         = "${var.cluster_name}/${each.key}/mcm_client_secret_b"
-  options      = { cas = 0 }
   disable_read = true
   data_json    = jsonencode({ value = "dummy" })
   lifecycle {
