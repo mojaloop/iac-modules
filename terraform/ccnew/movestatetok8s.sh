b894f80ae1c965829b48b73c3138200d9c179911
David Fry <david.fry@modusbox.com>
2024-07-29 23:56:56 +0100

    working version movetok8s

--- terraform/ccnew/movestatetok8s.sh
+++ terraform/ccnew/movestatetok8s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+set -e
 source setlocalvars.sh
-export output_dir=$(terragrunt run-all --terragrunt-exclude-dir k8s-deploy/ output ansible_output_dir | tr -d '"')
+export output_dir=$ANSIBLE_BASE_OUTPUT_DIR/k8s-deploy
 source $output_dir/gitlabenv.sh
 export KUBECONFIG_LOCATION=$output_dir/kubeconfig
 export K8S_STATE_NAMESPACE=gitlab
@@ -28,7 +28,11 @@ rm -rf /tmp/bootstrap || true
 git config --global user.email "root@{{ gitlab_fqdn }}"
 git config --global user.name "root"
 git clone https://root:${gitlab_access_token}@${gitlab_fqdn}/iac/bootstrap.git /tmp/bootstrap
-cp -rf ansible-k8s-deploy/ default-config/ k8s-deplooy/ *.sh terragrunt.hcl ../gitlab/ci-templates/bootstrap-v2/. /tmp/bootstrap
+cp -rf ansible-k8s-deploy/terragrunt.hcl /tmp/bootstrap/ansible-k8s-deploy
+cp -rf default-config/ /tmp/bootstrap
+cp -rf k8s-deploy/terragrunt.hcl /tmp/bootstrap/k8s-deploy
+cp -rf *.sh terragrunt.hcl /tmp/bootstrap
+cp -rf ../gitlab/ci-templates/bootstrap-v2/. /tmp/bootstrap
 cd /tmp/bootstrap
 git add .
 git commit -m "Push bootstrap files to GitLab"
