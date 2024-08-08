fc0f9ff807eaf93f94a89acd3363bfec6aafa2c8
Sijo George <sijogeorge5722@gmail.com>
2024-08-08 21:22:36 +0530

    removing root token secret

--- terraform/config-params/ccnew-config/deploy-env-config/vault-transit.tf
+++ terraform/config-params/ccnew-config/deploy-env-config/vault-transit.tf
@@ -27,7 +27,7 @@ path "${var.kv_path}/data/tenancy/*" {
 }
 
 path "auth/token/lookup-self" {
-    capabilities = ["read"]
+    capabilities = ["read","create","update"]
 }
 
 path "${vault_mount.transit.path}/encrypt/${vault_transit_secret_backend_key.unseal_key[each.value].name}" {
@@ -54,24 +54,6 @@ resource "vault_kv_secret_v2" "env_token" {
   data_json = jsonencode(
     {
       value = vault_token.env_token[each.value].client_token
-    }
-  )
-}
-
-resource "random_password" "vault_root_token" {
-  length           = 30
-  special          = true
-  override_special = "_"
-}
-
-
-resource "vault_kv_secret_v2" "vault_root_token" {
-  mount               = var.kv_path
-  name                = "tenancy/vault_root_token"
-  delete_all_versions = true
-  data_json = jsonencode(
-    {
-      value = random_password.vault_root_token.result
     }
   )
 }
