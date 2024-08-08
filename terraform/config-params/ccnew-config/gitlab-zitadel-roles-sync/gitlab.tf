f9897865e8d4042902eea81077e9792f3fb3310b
David Fry <david.fry@modusbox.com>
2024-08-08 18:50:49 +0100

    fix var

--- terraform/config-params/ccnew-config/gitlab-zitadel-roles-sync/gitlab.tf
+++ terraform/config-params/ccnew-config/gitlab-zitadel-roles-sync/gitlab.tf
@@ -20,17 +20,17 @@ locals {
   all_gitlab_users = { for gitlab_user in data.gitlab_users.all_users.users : gitlab_user.email => gitlab_user }
   need_to_add_admin_users_to_group = { for user_grant in data.zitadel_user_grants.active.user_grant_data : user_grant.email => {
     gitlab_user_id     = local.all_gitlab_users[user_grant.email].id
-    gitlab_group_perms = zitadel_to_gitlab_role_mapvar[var.admin_rbac_group]
+    gitlab_group_perms = local.zitadel_to_gitlab_role_map[var.admin_rbac_group]
   } if contains(keys(local.all_gitlab_users), user_grant.email) && contains(user_grant.role_keys, var.admin_rbac_group) }
 
   need_to_add_maintainer_users_to_group = { for user_grant in data.zitadel_user_grants.active.user_grant_data : user_grant.email => {
     gitlab_user_id     = local.all_gitlab_users[user_grant.email].id
-    gitlab_group_perms = zitadel_to_gitlab_role_mapvar[var.maintainer_rbac_group]
+    gitlab_group_perms = local.zitadel_to_gitlab_role_map[var.maintainer_rbac_group]
   } if contains(keys(local.all_gitlab_users), user_grant.email) && contains(user_grant.role_keys, var.maintainer_rbac_group) }
 
   need_to_add_developer_users_to_group = { for user_grant in data.zitadel_user_grants.active.user_grant_data : user_grant.email => {
     gitlab_user_id     = local.all_gitlab_users[user_grant.email].id
-    gitlab_group_perms = zitadel_to_gitlab_role_mapvar[var.user_rbac_group]
+    gitlab_group_perms = local.zitadel_to_gitlab_role_map[var.user_rbac_group]
   } if contains(keys(local.all_gitlab_users), user_grant.email) && contains(user_grant.role_keys, var.user_rbac_group) }
 
   zitadel_to_gitlab_role_map = {
