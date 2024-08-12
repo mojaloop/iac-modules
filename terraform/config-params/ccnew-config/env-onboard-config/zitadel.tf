265164452cf1af7a16ce9de338048989eb464e00
Sijo George <sijogeorge5722@gmail.com>
2024-08-12 21:41:39 +0530

    adding env onboard

---
+++ terraform/config-params/ccnew-config/env-onboard-config/zitadel.tf
@@ -0,0 +1,79 @@
+data "zitadel_orgs" "active" {
+  state = "ORG_STATE_ACTIVE"
+}
+
+data "zitadel_org" "default" {
+  for_each = toset(data.zitadel_orgs.active.ids)
+  id       = each.value
+}
+
+
+locals {
+  org_id = [for org in data.zitadel_org.default : org.id if org.is_default][0]
+}
+
+
+resource "zitadel_project" "env" {
+  name                   = var.env_name
+  org_id                 = local.org_id
+  project_role_assertion = true
+  project_role_check     = true
+}
+
+
+resource "zitadel_application_oidc" "grafana" {
+  project_id                  = zitadel_project.env.id
+  org_id                      = local.org_id
+  name                        = "${var.env_name}-grafana"
+  redirect_uris               = ["https://${var.grafana_fqdn}/login/generic_oauth"]
+  response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE", "OIDC_GRANT_TYPE_REFRESH_TOKEN"]
+  post_logout_redirect_uris   = ["https://${var.grafana_fqdn}"]
+  app_type                    = "OIDC_APP_TYPE_WEB"
+  auth_method_type            = "OIDC_AUTH_METHOD_TYPE_BASIC"
+  version                     = "OIDC_VERSION_1_0"
+  dev_mode                    = false
+  access_token_type           = "OIDC_TOKEN_TYPE_BEARER"
+  access_token_role_assertion = true
+  id_token_role_assertion     = true
+  id_token_userinfo_assertion = true
+  additional_origins          = []
+}
+
+resource "zitadel_application_oidc" "vault_ui" {
+  project_id                  = zitadel_project.env.id
+  org_id                      = local.org_id
+  name                        = "${var.env_name}-vault-ui"
+  redirect_uris               = ["https://${var.vault_fqdn}/ui/vault/auth/oidc/oidc/callback"]
+  response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
+  post_logout_redirect_uris   = ["https://${var.vault_fqdn}/ui"]
+  app_type                    = "OIDC_APP_TYPE_WEB"
+  auth_method_type            = "OIDC_AUTH_METHOD_TYPE_BASIC"
+  version                     = "OIDC_VERSION_1_0"
+  dev_mode                    = false
+  access_token_type           = "OIDC_TOKEN_TYPE_BEARER"
+  access_token_role_assertion = true
+  id_token_role_assertion     = true
+  id_token_userinfo_assertion = true
+  additional_origins          = []
+}
+
+resource "zitadel_application_oidc" "argocd" {
+  project_id                  = zitadel_project.env.id
+  org_id                      = local.org_id
+  name                        = "${var.env_name}-argocd"
+  redirect_uris               = ["https://${var.argocd_fqdn}/auth/callback"]
+  response_types              = ["OIDC_RESPONSE_TYPE_CODE"]
+  grant_types                 = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
+  post_logout_redirect_uris   = ["https://${var.argocd_fqdn}"]
+  app_type                    = "OIDC_APP_TYPE_WEB"
+  auth_method_type            = "OIDC_AUTH_METHOD_TYPE_BASIC"
+  version                     = "OIDC_VERSION_1_0"
+  dev_mode                    = false
+  access_token_type           = "OIDC_TOKEN_TYPE_BEARER"
+  access_token_role_assertion = true
+  id_token_role_assertion     = true
+  id_token_userinfo_assertion = true
+  additional_origins          = []
+}
