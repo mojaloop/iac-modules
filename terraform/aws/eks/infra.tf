676ffb4d64775b243bdf49c8373154b18b2c23f9
David Fry <david.fry@modusbox.com>
2024-09-12 11:44:06 +0100

    revert nft env change

--- terraform/aws/eks/infra.tf
+++ terraform/aws/eks/infra.tf
@@ -69,7 +69,6 @@ module "eks" {
           # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
           ENABLE_PREFIX_DELEGATION = "true"
           WARM_PREFIX_TARGET       = "1"
-          ENABLE_NFTABLES          = "true"
         }
       })
     }
