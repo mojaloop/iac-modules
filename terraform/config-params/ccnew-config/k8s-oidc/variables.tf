1e6a4720a0b745281812f182ac0b847ddfe48289
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-07 00:21:05 +0300

    updates for eks oidc

--- terraform/config-params/ccnew-config/k8s-oidc/variables.tf
+++ terraform/config-params/ccnew-config/k8s-oidc/variables.tf
@@ -21,3 +21,48 @@ variable "oidc_provider_group_claim_prefix" {
   type        = string
   description = "groups"
 }
+
+# EKS OIDC
+variable "k8s_cluster_type" {
+  type        = string
+  description = "Kubernetes cluster type"
+}
+
+variable "cluster_name" {
+    type        = string
+    description = "Name of the EKS Cluster"
+}
+
+variable "identity_provider_config_name" {
+    type        = string
+    description = "The name of the identity provider config"
+}
+
+variable "kubernetes_oidc_issuer" {
+    type        = string
+    description = "Issuer URL for the OpenID Connect identity provider"
+}
+
+variable "kubernetes_oidc_groups_claim" {
+    type        = string
+    description = "The JWT claim that the provider will use to return groups"
+    default     = "groups"
+}
+
+variable "kubernetes_oidc_groups_prefix" {
+    type        = string
+    description = "A prefix that is prepended to group claims"
+    default     = "oidc:"
+}
+
+variable "kubernetes_oidc_username_claim" {
+    type        = string
+    description = "The JWT claim that the provider will use as the username"
+    default     = "username"
+}
+
+variable "kubernetes_oidc_username_prefix" {
+    type        = string
+    description = "A prefix that is prepended to username claims"
+    default     = "oidc:"
+}
