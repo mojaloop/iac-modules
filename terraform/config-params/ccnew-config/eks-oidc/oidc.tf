dcd832b045300ca015fc359a9a82badac4d068fa
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-09 18:58:33 +0300

    revert to no Interpolation

--- terraform/config-params/ccnew-config/eks-oidc/oidc.tf
+++ terraform/config-params/ccnew-config/eks-oidc/oidc.tf
@@ -89,10 +89,10 @@ resource "aws_eks_identity_provider_config" "eks_cluster" {
 
   oidc {
     client_id                     = zitadel_application_oidc.eks_cli.client_id
-    identity_provider_config_name = "${var.identity_provider_config_name}"
+    identity_provider_config_name = var.identity_provider_config_name
     issuer_url                    = "https://${var.zitadel_fqdn}"
-    username_claim                = "${var.kubernetes_oidc_username_claim}"
-    groups_claim                  = "${var.kubernetes_oidc_groups_claim}"
+    username_claim                = var.kubernetes_oidc_username_claim
+    groups_claim                  = var.kubernetes_oidc_groups_claim
     #username_prefix              = var.kubernetes_oidc_username_prefix
     #groups_prefix                = var.kubernetes_oidc_groups_prefix
   }
