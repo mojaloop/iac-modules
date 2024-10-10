1bcfb6d5813b28d1b0fd9b654b5e98fb5d720c40
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-10 12:27:51 +0300

    aws_eks_identity_provider_config specific outputs

--- terraform/config-params/ccnew-config/eks-oidc/outputs.tf
+++ terraform/config-params/ccnew-config/eks-oidc/outputs.tf
@@ -1,12 +1,7 @@
-output "kubernetes_oidc_client_id" {
-  value     = zitadel_application_oidc.eks_cli.client_id
-  sensitive = true
+output "eks_identity_provider_status" {
+  value     = aws_eks_identity_provider_config.eks_cluster.status
 }
 
-output "k8s_project_id" {
-  value = zitadel_project.eks.id
-}
-
-output "kubernetes_oidc_issuer" {
-  value = "https://${var.zitadel_fqdn}"
+output "eks_identity_provider_id" {
+  value     = aws_eks_identity_provider_config.eks_cluster.id
 }
