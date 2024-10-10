7aa36bc5f39ea2847cee52579e6ef86ebb920385
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-10 12:28:12 +0300

    clean eks oidc module to re-use k8s module for most parts

--- terraform/config-params/ccnew-config/eks-oidc/variables.tf
+++ terraform/config-params/ccnew-config/eks-oidc/variables.tf
@@ -3,20 +3,6 @@ variable "zitadel_fqdn" {
   type        = string
 }
 
-variable "admin_rbac_group" {
-  type        = string
-  description = "rbac group in idm for admin access via oidc"
-}
-
-variable "user_rbac_group" {
-  type        = string
-  description = "rbac group in idm for user access via oidc"
-}
-
-variable "zitadel_admin_human_user_id" {
-  description = "admin zitadel human user id"
-}
-
 variable "oidc_provider_group_claim_prefix" {
   type        = string
   description = "groups"
@@ -55,4 +41,9 @@ variable "kubernetes_oidc_username_prefix" {
     type        = string
     description = "A prefix that is prepended to username claims"
     default     = ""
+}
+
+variable "kubernetes_oidc_client_id" {
+  type          = string
+  description   = "Zitadel client ID for k8s"
 }
