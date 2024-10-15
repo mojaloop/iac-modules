2255f12642ba308795d87814161cd3608a4b05b4
David Fry <david.fry@modusbox.com>
2024-10-15 17:10:51 +0100

    user iam user for cert man

--- terraform/aws/config-params/ccnew-config/vault-post-config/vault.tf
+++ terraform/aws/config-params/ccnew-config/vault-post-config/vault.tf
@@ -33,6 +33,14 @@ resource "vault_aws_secret_backend_role" "dns_access" {
   role_arns       = [var.external_dns_cloud_role]
 }
 
+resource "vault_aws_secret_backend_role" "cert_manager_access" {
+  count           = local.enable_dns_backend ? 1 : 0
+  backend         = vault_aws_secret_backend.aws_dns[0].path
+  name            = var.cert_manager_access_role
+  credential_type = "iam_user"
+  policy_arns     = [var.cert_manager_cloud_policy]
+}
+
 data "vault_generic_secret" "credentials" {
   path = "${var.kv_path}/${var.credential_path}"
 }
