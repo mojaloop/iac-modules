29190b3c449b342f8f8840fe38b629729a43099c
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-18 10:52:55 +0300

    comment bastion_hosts_yaml_maps

--- terraform/aws/eks/outputs.tf
+++ terraform/aws/eks/outputs.tf
@@ -158,12 +158,12 @@ output "bastion_hosts_var_maps" {
   }
 }
 
-output "bastion_hosts_yaml_maps" {
-  sensitive = false
-  value = {
-    eks_post_install_config_map = replace(module.eks.aws_auth_configmap_yaml, "{{", "{{ '{{' }}")
-  }
-}
+# output "bastion_hosts_yaml_maps" {
+#   sensitive = false
+#   value = {
+#     eks_post_install_config_map = replace(module.eks.aws_auth_configmap_yaml, "{{", "{{ '{{' }}")
+#   }
+# }
 
 
 
