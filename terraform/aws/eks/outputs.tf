329bd4a5691544df6bf88344c54a83ed580c8bba
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-19 12:46:12 +0300

    try empty bastion_hosts_yaml_maps

--- terraform/aws/eks/outputs.tf
+++ terraform/aws/eks/outputs.tf
@@ -160,10 +160,10 @@ output "bastion_hosts_var_maps" {
 
 output "bastion_hosts_yaml_maps" {
   sensitive = false
-  value = {
-    #eks_post_install_config_map = replace(module.eks.aws_auth_configmap_yaml, "{{", "{{ '{{' }}")
-    eks_post_install_config_map = {}
-  }
+  value = {}
+  # value = {
+  #   eks_post_install_config_map = replace(module.eks.aws_auth_configmap_yaml, "{{", "{{ '{{' }}")
+  # }
 }
 
 output "cluster_iam_role_arn" {
