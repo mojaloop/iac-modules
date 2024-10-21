10e07e9e884f831272c13c89cfb67686e2a6d5c2
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2024-10-21 12:41:24 +0530

    adding iops and storage type

--- terraform/aws/support-svcs/deploy-rds/infra.tf
+++ terraform/aws/support-svcs/deploy-rds/infra.tf
@@ -16,6 +16,8 @@ module "rds" {
   instance_class      = each.value.external_resource_config.instance_class
   allocated_storage   = each.value.external_resource_config.allocated_storage
   storage_encrypted   = each.value.external_resource_config.storage_encrypted
+  storage_type        = each.value.external_resource_config.storage_type
+  ops                 = each.value.external_resource_config.storage_iops
   multi_az            = each.value.external_resource_config.multi_az
   skip_final_snapshot = each.value.external_resource_config.skip_final_snapshot
 
