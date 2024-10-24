8b2bb9f03278fef22a01fb579c0709e19e0732bb
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-25 00:42:10 +0300

    output root account number

--- terraform/aws/eks_cc/outputs.tf
+++ terraform/aws/eks_cc/outputs.tf
@@ -139,7 +139,7 @@ output "all_hosts_var_maps" {
     kubeapi_loadbalancer_fqdn    = module.eks.cluster_endpoint
     eks_cluster_name             = module.eks.cluster_name
     eks_access_role_arn          = aws_iam_role.eks_access_role.arn
-    #eks_managed_role_arns        = module.eks.self_managed_node_groups
+    root_account_id              = data.aws_caller_identity.current.account_id
   }
 }
 
