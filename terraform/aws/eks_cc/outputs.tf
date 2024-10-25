37eae7938920c1a5c4095c743077530e0bc69068
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-25 15:48:17 +0300

    use sub-mod

--- terraform/aws/eks_cc/outputs.tf
+++ terraform/aws/eks_cc/outputs.tf
@@ -202,3 +202,26 @@ output "private_subnets" {
 output "vpc_id" {
   value = module.base_infra.vpc_id
 }
+
+locals {
+  eks_name                = substr(replace(local.base_domain, ".", "-"), 0, 16)
+  eks_user_arns = distinct([
+    module.post_config.ci_user_arn,
+    data.aws_caller_identity.current_user.arn
+  ])
+  
+locals {
+  aws_auth_configmap_yaml = templatefile("${path.module}/templates/aws_auth_cm.tpl",
+    {
+      node_iam_role_arns = distinct(
+        compact(
+          concat(
+            [for group in module.eks.eks_managed_node_group : group.iam_role_arn if group.platform != "windows"],
+            [for group in module.eks.eks_managed_node_group : group.iam_role_arn if group.platform != "windows"]
+          )
+        )
+      ),
+      iam_user_role_arns = [aws_iam_role.eks_access_role.arn]
+    }
+  )
+}
