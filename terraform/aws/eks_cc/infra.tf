c7631e67a4c33e8b9dac2ccf9c6acc9b08d84e3b
Josphat Mutai <josphatkmutai@gmail.com>
2024-10-25 11:09:47 +0300

    eks cc first draft

---
+++ terraform/aws/eks_cc/infra.tf
@@ -0,0 +1,296 @@
+module "ubuntu_focal_ami" {
+  source  = "../ami-ubuntu"
+  release = "20.04"
+}
+
+module "base_infra" {
+  source                     = "../base-infra"
+  cluster_name               = var.cluster_name
+  domain                     = var.domain
+  tags                       = var.tags
+  vpc_cidr                   = var.vpc_cidr
+  create_public_zone         = var.create_public_zone
+  create_private_zone        = var.create_private_zone
+  manage_parent_domain       = var.manage_parent_domain
+  manage_parent_domain_ns    = var.manage_parent_domain_ns
+  az_count                   = var.az_count
+  block_size                 = var.block_size
+  route53_zone_force_destroy = var.dns_zone_force_destroy
+  bastion_ami                = module.ubuntu_focal_ami.id
+  create_haproxy_dns_record  = true
+}
+
+module "post_config" {
+  source                      = "../post-config-k8s"
+  name                        = var.cluster_name
+  domain                      = var.domain
+  tags                        = var.tags
+  private_zone_id             = module.base_infra.public_int_zone.id
+  public_zone_id              = module.base_infra.public_zone.id
+  create_ext_dns_user         = var.create_ext_dns_user
+  create_ext_dns_role         = var.create_ext_dns_role
+  create_iam_user             = var.create_ci_iam_user
+  iac_group_name              = var.iac_group_name
+  backup_bucket_name          = "${var.domain}-${var.backup_bucket_name}"
+  backup_enabled              = var.backup_enabled
+  backup_bucket_force_destroy = var.backup_bucket_force_destroy
+}
+
+module "k6s_test_harness" {
+  count                       = var.enable_k6s_test_harness ? 1 : 0
+  source                      = "../k6s-test-harness"
+  cluster_name                = var.cluster_name
+  domain                      = var.domain
+  tags                        = var.tags
+  vpc_cidr                    = var.vpc_cidr
+  vpc_id                      = module.base_infra.vpc_id
+  ami_id                      = module.ubuntu_focal_ami.id
+  docker_server_instance_type = var.k6s_docker_server_instance_type
+  subnet_id                   = module.base_infra.private_subnets[0]
+  key_pair_name               = module.base_infra.key_pair_name
+  public_zone_id              = module.base_infra.public_zone.id
+  test_harness_hostname       = var.k6s_docker_server_fqdn
+}
+
+module "eks" {
+  source      = "terraform-aws-modules/eks/aws"
+  version     = "~> 19.21"
+
+  enable_irsa = true
+
+  cluster_name                    = local.eks_name
+  cluster_version                 = var.kubernetes_version
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = false
+  
+  # Enable the default key policy (no need for kms_key_administrators or kms_key_owners)
+  kms_key_enable_default_policy   = true
+
+  # EKS cluster access entry - aws-auth configmap is deprecated (from module =>20)
+  ## Needs terraform >=1.3.2
+  # enable_cluster_creator_admin_permissions = true
+  # authentication_mode = "API_AND_CONFIG_MAP"
+  # access_entries = {
+  #   eks_access_role = {
+  #     kubernetes_groups = []
+  #     principal_arn     = aws_iam_role.eks_access_role.arn
+  #     type              = "STANDARD"
+      
+  #     policy_associations = {
+  #       admin_access = {
+  #         policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  #         access_scope = {
+  #           namespaces = []
+  #           type = "cluster"
+  #         }
+  #       }
+  #     }
+  #   }
+  # }
+  
+  vpc_id     = module.base_infra.vpc_id
+  subnet_ids = module.base_infra.private_subnets
+  cluster_addons = {
+    vpc-cni = {
+      # Specify the VPC CNI addon should be deployed before compute to ensure
+      # the addon is configured before data plane compute resources are created
+      # See README for further details
+      before_compute = true
+      most_recent    = true # To ensure access to the latest settings provided
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION           = "true"
+          WARM_PREFIX_TARGET                 = "1"
+          AWS_VPC_K8S_CNI_EXCLUDE_SNAT_CIDRS = "${var.netbird_ip_range},${var.cc_cidr_block}"
+        }
+      })
+    }
+  }
+  cluster_security_group_additional_rules = {
+
+    ingress_https_bastion = {
+      description              = "Access EKS from Bastion instance."
+      protocol                 = "tcp"
+      from_port                = 443
+      to_port                  = 443
+      type                     = "ingress"
+      source_security_group_id = module.base_infra.bastion_security_group_id
+    }
+  }
+  # Self Managed Node Group(s)
+  self_managed_node_group_defaults = {
+    update_launch_template_default_version = true
+    autoscaling_group_tags = {
+      "k8s.io/cluster-autoscaler/enabled" : true,
+      "k8s.io/cluster-autoscaler/${local.eks_name}" : "owned",
+    }
+  }
+  # Conditionally include OIDC configuration if var.enable_oidc is true
+  cluster_identity_providers = var.eks_oidc_enabled ? {
+    oidc = {
+      identity_provider_config_name = var.identity_provider_config_name
+      issuer_url                    = var.kubernetes_oidc_issuer
+      client_id                     = var.kubernetes_oidc_client_id
+      groups_claim                  = var.kubernetes_oidc_groups_claim
+      #groups_prefix                = var.kubernetes_oidc_groups_prefix
+      username_claim = var.kubernetes_oidc_username_claim
+      #username_prefix              = var.kubernetes_oidc_username_prefix
+    }
+  } : {}
+
+  self_managed_node_groups = local.self_managed_node_groups
+  tags                     = var.tags
+}
+
+# CICD user role for cluster access
+resource "aws_iam_role" "eks_access_role" {
+  name = "${local.eks_name}-eks-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = local.eks_user_arns
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+locals {
+  eks_name                = substr(replace(local.base_domain, ".", "-"), 0, 16)
+  eks_user_arns = distinct([
+    module.post_config.ci_user_arn,
+    data.aws_caller_identity.current_user.arn
+  ])
+  base_security_groups    = [aws_security_group.self.id, module.base_infra.default_security_group_id]
+  traffic_security_groups = [aws_security_group.ingress.id]
+  kubeapi_target_groups = [
+    aws_lb_target_group.internal_kubeapi.arn
+  ]
+  traffic_target_groups = [
+    aws_lb_target_group.external_http.arn,
+    aws_lb_target_group.external_https.arn,
+    aws_lb_target_group.internal_http.arn,
+    aws_lb_target_group.internal_https.arn,
+    aws_lb_target_group.wireguard.arn
+  ]
+  master_target_groups   = var.master_node_supports_traffic ? concat(local.kubeapi_target_groups, local.traffic_target_groups) : local.kubeapi_target_groups
+  agent_target_groups    = local.traffic_target_groups
+  master_security_groups = var.master_node_supports_traffic ? concat(local.base_security_groups, local.traffic_security_groups) : local.base_security_groups
+  agent_security_groups  = concat(local.base_security_groups, local.traffic_security_groups)
+  node_labels = { for node_pool_key, node_pool in var.node_pools :
+    node_pool_key => {
+      extra_args = [for key, label in node_pool.node_labels : "${key}=${label}"]
+    }
+  }
+  node_taints = { for node_pool_key, node_pool in var.node_pools :
+    node_pool_key => {
+      extra_args = [for key, taint in node_pool.node_taints : "${taint}"]
+    }
+  }
+
+  self_managed_node_groups = { for node_pool_key, node_pool in var.node_pools :
+    node_pool_key => {
+      name                            = "${local.eks_name}-${node_pool_key}"
+      ami_id                          = data.aws_ami.eks_default.id
+      instance_type                   = node_pool.instance_type
+      public_ip                       = false
+      max_size                        = node_pool.node_count
+      desired_size                    = node_pool.node_count
+      use_mixed_instances_policy      = false
+      target_group_arns               = local.agent_target_groups
+      key_name                        = module.base_infra.key_pair_name
+      launch_template_name            = "${local.eks_name}-${node_pool_key}"
+      launch_template_use_name_prefix = false
+      iam_role_name                   = "${local.eks_name}-${node_pool_key}"
+      iam_role_use_name_prefix        = false
+      vpc_security_group_ids = [
+        module.eks.cluster_primary_security_group_id
+      ]
+      bootstrap_extra_args     = "--use-max-pods false --kubelet-extra-args '--max-pods=110 --node-labels=${join(",", local.node_labels[node_pool_key].extra_args)} --register-with-taints=${join(",", local.node_taints[node_pool_key].extra_args)}'"
+      post_bootstrap_user_data = "${data.template_file.post_bootstrap_user_data.rendered}"
+      ebs_optimized            = true
+      block_device_mappings = merge(
+        {
+          xvda = {
+            device_name = "/dev/xvda"
+            ebs = {
+              volume_size           = node_pool.storage_gbs
+              volume_type           = "gp3"
+              iops                  = 3000
+              throughput            = 150
+              encrypted             = true
+              delete_on_termination = true
+            }
+          }
+        },
+        node_pool.extra_vol ? {
+          xvdb = {
+            device_name = "/dev/xvdb"
+            ebs = {
+              volume_size           = node_pool.storage_gbs
+              volume_type           = "gp3"
+              iops                  = 3000
+              throughput            = 150
+              encrypted             = true
+              delete_on_termination = true
+            }
+          }
+        } : {}
+      )
+
+      network_interfaces = [
+        {
+          delete_on_termination = true
+          security_groups       = local.agent_security_groups
+        }
+      ]
+
+      tags = merge(
+        { Name = "${local.eks_name}-${node_pool_key}" },
+        local.common_tags
+      )
+
+      tag_specifications = ["instance", "volume", "network-interface"]
+    }
+  }
+}
+
+data "aws_caller_identity" "current_user" {}
+
+data "template_file" "post_bootstrap_user_data" {
+  template = file("${path.module}/templates/post-bootstrap-user-data.sh.tpl")
+
+  vars = {
+    netbird_version   = var.netbird_version
+    netbird_api_host  = var.netbird_api_host
+    netbird_setup_key = var.netbird_setup_key
+  }
+}
+
+data "aws_ami" "eks_default" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-${var.kubernetes_version}-v*"]
+  }
+}
+
+data "aws_ami" "eks_ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-eks/k8s_${var.kubernetes_version}/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+}
+#-register-with-taints=spotInstance=true:PreferNoSchedule add for taints in bootstrap_extra_args
---
+++ terraform/aws/eks_cc/outputs.tf
@@ -0,0 +1,197 @@
+output "nat_public_ips" {
+  description = "nat gateway public ips"
+  value       = module.base_infra.nat_public_ips
+}
+
+output "internal_load_balancer_dns" {
+  value = aws_lb.internal.dns_name
+}
+
+output "external_load_balancer_dns" {
+  value = aws_lb.lb.dns_name
+}
+
+output "private_subdomain" {
+  value = module.base_infra.public_int_zone.name
+}
+
+output "public_subdomain" {
+  value = module.base_infra.public_zone.name
+}
+
+output "internal_interop_switch_fqdn" {
+  value = "${var.int_interop_switch_subdomain}.${trimsuffix(module.base_infra.public_zone.name, ".")}"
+}
+
+output "external_interop_switch_fqdn" {
+  value = "${var.ext_interop_switch_subdomain}.${trimsuffix(module.base_infra.public_zone.name, ".")}"
+}
+
+output "target_group_internal_https_port" {
+  value = var.target_group_internal_https_port
+}
+output "target_group_internal_http_port" {
+  value = var.target_group_internal_http_port
+}
+output "target_group_external_https_port" {
+  value = var.target_group_external_https_port
+}
+output "target_group_external_http_port" {
+  value = var.target_group_external_http_port
+}
+
+output "target_group_internal_health_port" {
+  value = var.target_group_internal_health_port
+}
+output "target_group_external_health_port" {
+  value = var.target_group_external_health_port
+}
+output "target_group_vpn_port" {
+  value = var.wireguard_port
+}
+output "private_network_cidr" {
+  value = var.vpc_cidr
+}
+
+output "internal_k8s_network_cidr" {
+  value = module.base_infra.private_subnets_cidr_blocks
+}
+
+output "ext_dns_cloud_policy" {
+  value = module.post_config.ext_dns_cloud_policy
+}
+
+output "external_dns_cloud_role" {
+  value = module.post_config.external_dns_cloud_role
+}
+
+output "object_storage_cloud_role" {
+  value = module.post_config.object_storage_cloud_role
+}
+output "object_storage_bucket_name" {
+  value = module.post_config.backup_bucket_name
+}
+
+
+###new items
+
+output "bastion_ssh_key" {
+  sensitive = true
+  value     = module.base_infra.ssh_private_key
+}
+
+output "bastion_private_ip" {
+  value = module.base_infra.bastion_private_ip
+}
+output "bastion_private_ips" {
+  value = module.base_infra.bastion_private_ips
+}
+output "bastion_public_ip" {
+  value = module.base_infra.bastion_public_ip
+}
+output "bastion_public_ips" {
+  value = module.base_infra.bastion_public_ips
+}
+output "bastion_hosts" {
+  value = zipmap([for i in range(length(module.base_infra.bastion_public_ips)) : "bastion${i + 1}"], module.base_infra.bastion_public_ips)
+}
+output "bastion_os_username" {
+  value = var.os_user_name
+}
+
+output "dns_provider" {
+  value = var.dns_provider
+}
+
+output "master_hosts_var_maps" {
+  value = {}
+}
+
+output "master_hosts_yaml_maps" {
+  value = {}
+}
+
+output "secrets_var_map" {
+  sensitive = true
+  value     = module.post_config.secrets_var_map
+}
+
+output "properties_var_map" {
+  value = module.post_config.properties_var_map
+}
+
+output "properties_key_map" {
+  value = module.post_config.post_config_properties_key_map
+}
+
+output "secrets_key_map" {
+  value = module.post_config.post_config_secrets_key_map
+}
+
+output "all_hosts_var_maps" {
+  sensitive = false
+  value = {
+    ansible_ssh_user             = var.os_user_name
+    ansible_ssh_retries          = "10"
+    base_domain                  = local.base_domain
+    internal_interop_switch_fqdn = "${var.int_interop_switch_subdomain}.${trimsuffix(module.base_infra.public_zone.name, ".")}"
+    external_interop_switch_fqdn = "${var.ext_interop_switch_subdomain}.${trimsuffix(module.base_infra.public_zone.name, ".")}"
+    kubeapi_loadbalancer_fqdn    = module.eks.cluster_endpoint
+    eks_cluster_name             = module.eks.cluster_name
+    eks_access_role_arn          = aws_iam_role.eks_access_role.arn
+    root_account_id              = data.aws_caller_identity.current_user.account_id
+  }
+}
+
+output "agent_hosts_var_maps" {
+  sensitive = false
+  value     = {}
+}
+
+output "agent_hosts_yaml_maps" {
+  value = {}
+}
+
+output "bastion_hosts_var_maps" {
+  sensitive = false
+  value = {
+    ansible_ssh_common_args = "-o StrictHostKeyChecking=no"
+    egress_gateway_cidr     = var.vpc_cidr
+  }
+}
+
+output "bastion_hosts_yaml_maps" {
+  sensitive = false
+  value = {
+    eks_post_install_config_map = replace(module.eks.aws_auth_configmap_yaml, "{{", "{{ '{{' }}")
+  }
+}
+
+output "ci_user_arn" {
+  value = module.post_config.ci_user_arn
+}
+
+output "agent_hosts" {
+  value = {}
+}
+
+output "master_hosts" {
+  value = {}
+}
+
+output "test_harness_hosts" {
+  value = var.enable_k6s_test_harness ? { test_harness = module.k6s_test_harness[0].test_harness_private_ip } : {}
+}
+
+output "test_harness_hosts_var_maps" {
+  value = var.enable_k6s_test_harness ? module.k6s_test_harness[0].var_map : {}
+}
+
+output "private_subnets" {
+  # value = "[${join(",", [for s in module.base_infra.private_subnets : format("'%s'", s)])}]"
+  value = module.base_infra.private_subnets
+}
+
+output "vpc_id" {
+  value = module.base_infra.vpc_id
+}
