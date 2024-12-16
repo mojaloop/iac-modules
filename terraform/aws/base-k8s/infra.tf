11607c1e31fc0f7e7f049a4621ab36635bcf4bb8
Josphat Mutai <josphatkmutai@gmail.com>
2024-12-16 23:57:05 +0300

    another try import

--- terraform/aws/base-k8s/infra.tf
+++ terraform/aws/base-k8s/infra.tf
@@ -239,5 +239,5 @@ locals {
 import {
   for_each = var.backup_bucket_import_enabled ? [1] : []
   id  = "${var.domain}-${var.backup_bucket_name}"
-  to  = module.post_config.backup_bucket
+  to  = module.post_config.aws_s3_bucket.backup_bucket
 }
