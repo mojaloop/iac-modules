b1e6bae4459bb51a012f39567d8aa4d26961512c
muzammil360 <muzammil360@gmail.com>
2024-12-17 16:52:24 +0500

    add vault secret backend and role

--- terraform/aws/config-params/ccnew-config/vault-post-config/vault.tf
+++ terraform/aws/config-params/ccnew-config/vault-post-config/vault.tf
@@ -41,6 +41,24 @@ resource "vault_aws_secret_backend_role" "cert_manager_access" {
   policy_arns     = [var.cert_manager_cloud_policy]
 }
 
+resource "vault_aws_secret_backend" "cloudwatch" {
+  count                     = 1                              # TODO: make it configurable
+  path                      = "cc-cloud-provider-cloudwatch" # TODO: make it configurable
+  access_key                = data.vault_generic_secret.credentials.data[var.access_key_name]
+  secret_key                = data.vault_generic_secret.credentials.data[var.secret_key_name]
+  region                    = var.region
+  default_lease_ttl_seconds = var.default_lease_ttl_seconds
+}
+
+resource "vault_aws_secret_backend_role" "cloudwatch" {
+  count           = 1 # TODO: make it configurable
+  backend         = vault_aws_secret_backend.cloudwatch[0].path
+  name            = "cloudwatch-reader" # TODO: make it configurable
+  credential_type = "assumed_role"
+  # TODO: make it configurable
+  role_arns = ["arn:aws:iam::390402568199:role/test02.ccv2.drpp.global-cloudwatch-readonly"]
+}
+
 data "vault_generic_secret" "credentials" {
   path = "${var.kv_path}/${var.credential_path}"
 }
