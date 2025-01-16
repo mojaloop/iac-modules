379b860126a494963e4c58959da7d56da858d145
Josphat Mutai <josphatkmutai@gmail.com>
2025-01-16 14:00:20 +0300

    Add registry_mirror_fqdn var

--- terraform/aws/eks/infra.tf
+++ terraform/aws/eks/infra.tf
@@ -282,7 +282,8 @@ data "template_file" "post_bootstrap_user_data" {
     netbird_setup_key = var.netbird_setup_key
     pod_network_cidr  = var.vpc_cidr
     container_registry_mirrors = join(" ", var.container_registry_mirrors)
-    enable_registry_mirror = var.enable_registry_mirror
+    enable_registry_mirror     = var.enable_registry_mirror
+    registry_mirror_fqdn       = var.registry_mirror_fqdn
   }
 }
 
