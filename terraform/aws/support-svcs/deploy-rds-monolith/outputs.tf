9742f85ffce1c6fe64fa9b679f4b40c693a6cf45
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-02-04 14:23:54 +0530

    change in structure

--- terraform/aws/support-svcs/deploy-rds-monolith/outputs.tf
+++ terraform/aws/support-svcs/deploy-rds-monolith/outputs.tf
@@ -1,6 +1,6 @@
 output "secrets_var_map" {
   sensitive = true
-  value = { for index, rds_module in module.rds :  var.rds_services[index].external_resource_config.password_key_name => rds_module.db_instance_master_user_password }
+  value =  { for index, int_database in var.monolith_internal_databases : int_database.external_resource_config.password_key_name => random_password.rds_user_password[index].result}
 }
 
 output "properties_var_map" {
