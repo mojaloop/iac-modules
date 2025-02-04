d8e96fbcf7da37ec604bddf82b14571cd0e36ec2
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-02-04 12:17:51 +0530

    expression change

--- terraform/aws/support-svcs/deploy-rds/outputs.tf
+++ terraform/aws/support-svcs/deploy-rds/outputs.tf
@@ -1,14 +1,6 @@
 output "secrets_var_map" {
   sensitive = true
-  value = 
-    var.managed_svc_as_monolith ?
-     { for index, int_database in var.monolith_internal_databases : 
-       int_database.external_resource_config.password_key_name => random_password.rds_user_password[index].result
-     }
-    :{
-    for index, rds_module in module.rds : 
-      var.rds_services[index].external_resource_config.password_key_name => rds_module.db_instance_master_user_password
-  }
+  value =  var.managed_svc_as_monolith ? { for index, int_database in var.monolith_internal_databases : int_database.external_resource_config.password_key_name => random_password.rds_user_password[index].result} : { for index, rds_module in module.rds :  var.rds_services[index].external_resource_config.password_key_name => rds_module.db_instance_master_user_password }
 }
 
 output "properties_var_map" {
