4f0d8f8bf8f1b4311e0948081bc10a72eff29c23
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-02-05 13:41:07 +0530

    expression

--- terraform/aws/support-svcs/deploy-rds-monolith/outputs.tf
+++ terraform/aws/support-svcs/deploy-rds-monolith/outputs.tf
@@ -6,7 +6,7 @@ output "secrets_var_map" {
 output "properties_var_map" {
   value = {
     for index, int_database in var.monolith_internal_databases : 
-      int_database.external_resource_config.instance_address_key_name => module.rds[each.value.external_resource_config.monolith_db_server].db_instance_address
+      int_database.external_resource_config.instance_address_key_name => module.rds[int_database.external_resource_config.monolith_db_server].db_instance_address
   }
 }
 
