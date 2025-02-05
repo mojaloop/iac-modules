db5100ace5a7ce69a9b9c56033866e739d8db05a
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-02-05 13:36:43 +0530

    output

--- terraform/aws/support-svcs/deploy-rds-monolith/outputs.tf
+++ terraform/aws/support-svcs/deploy-rds-monolith/outputs.tf
@@ -5,14 +5,14 @@ output "secrets_var_map" {
 
 output "properties_var_map" {
   value = {
-    for index, rds_module in module.rds : 
-      var.rds_services[index].external_resource_config.instance_address_key_name => rds_module.db_instance_address
+    for index, int_database in var.monolith_internal_databases : 
+      int_database.external_resource_config.instance_address_key_name => module.rds[each.value.external_resource_config.monolith_db_server].db_instance_address
   }
 }
 
 output "secrets_key_map" {
   value = {
-    for index, rds_module in module.rds : 
-      var.rds_services[index].external_resource_config.password_key_name => var.rds_services[index].external_resource_config.password_key_name
+    or index, int_database in var.monolith_internal_databases : 
+      int_database.external_resource_config.password_key_name => int_database.external_resource_config.password_key_name
   }
 }
