ebcb7fbef27f2c59657689110e3701e872201e43
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-02-21 16:53:27 +0530

    cloudstorage prereq

---
+++ terraform/config-params/ccnew-config/env-onboard-cloudstorage-pre/variables.tf
@@ -0,0 +1,11 @@
+variable "cc_cluster_name" {
+  description = "control center cluster name"
+}
+
+variable "env_name" {
+  description = "name of environment"
+}
+
+variable "kv_path" {
+  description = "key value secret path"
+}
