6768559dbd6fc5fd61973c1640118b83af4bf596
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-02-21 18:14:42 +0530

    change

--- terraform/gitops/k8s-cluster-config/storage-config.tf
+++ terraform/gitops/k8s-cluster-config/storage-config.tf
@@ -5,6 +5,10 @@ module "generate_storage_files" {
     csi_driver_replicas             = 2
     kubelet_dir_path                = "/var/lib/kubelet"
     storage_controlplane_namespace  = "kube-system"
+    storage_class_name              = var.storage_class_name
+    access_secret_name              = "aws_ebs_csi_cred"
+    access_key_id                   = "block_storage_secret_key_id"
+    secret_access_key               = "block_storage_secret_access_key"
   }
   file_list       = [for f in fileset(local.storage_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.storage_app_file, f))]
   template_path   = local.storage_template_path
