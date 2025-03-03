7180bd68bf5633b29a9d4d26c75f81728332af51
Josphat Mutai <josphatkmutai@gmail.com>
2025-03-03 19:43:58 +0300

    clean up

--- terraform/aws/base-infra/variables.tf
+++ terraform/aws/base-infra/variables.tf
@@ -138,7 +138,6 @@ locals {
   public_zone                   = var.configure_route_53 ? (var.create_public_zone ? aws_route53_zone.public[0] : data.aws_route53_zone.public[0]) : null
   private_zone                  = var.configure_route_53 ? (var.create_private_zone ? aws_route53_zone.private[0] : data.aws_route53_zone.private[0]) : null
   public_int_zone               = var.configure_route_53 ? (var.create_public_zone ? aws_route53_zone.public_int[0] : data.aws_route53_zone.public_int[0]) : null
-  # cluster_parent_zone_id        = var.configure_route_53 ? (var.manage_parent_domain ? aws_route53_zone.cluster_parent[0].zone_id : data.aws_route53_zone.cluster_parent[0].zone_id) : null
   cluster_parent_zone_id        = length(data.aws_route53_zone.cluster_parent) > 0 ? data.aws_route53_zone.cluster_parent[0].zone_id : aws_route53_zone.cluster_parent[0].zone_id
   cluster_parent_parent_zone_id = var.configure_route_53 ? ((var.manage_parent_domain && var.manage_parent_domain_ns) ? data.aws_route53_zone.cluster_parent_parent[0].zone_id : null) : null
   ssh_keys                      = []
