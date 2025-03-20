eb1c6334b21e9ff6276554de4893223bc3901225
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-03-20 15:59:45 +0530

    db secret

---
+++ terraform/config-params/ccnew-config/env-onboard-config/db-secrets.tf
@@ -0,0 +1,87 @@
+variable "common_platform_db_secret_name" {
+}
+
+variable "common_mojaloop_db_secret_name" {
+}
+
+variable "common_platform_db_external_name" {
+}
+
+variable "common_mojaloop_db_external_name"{
+}
+
+data "kubernetes_secret_v1" "common_platform_db_secret" {
+  metadata {
+      name      = var.common_platform_db_secret_name
+      namespace = var.env_name
+  }
+}
+
+data "kubernetes_secret_v1" "common_mojaloop_db_secret" {
+  metadata {
+      name      = var.common_mojaloop_db_secret_name
+      namespace = var.env_name
+  }
+}
+
+resource "vault_kv_secret_v2" "common_platform_db_secret" {
+  mount               = var.kv_path
+  name                = "${var.env_name}/common_platform_db_secret"
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      value = try(data.kubernetes_secret_v1.common_platform_db_secret.data.password, "")
+    }
+  )
+}
+
+
+resource "vault_kv_secret_v2" "common_mojaloop_db_secret" {
+  mount               = var.kv_path
+  name                = "${var.env_name}/common_mojaloop_db_secret"
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      value = try(data.kubernetes_secret_v1.common_mojaloop_db_secret.data.password, "")
+    }
+  )
+}
+
+
+data "kubernetes_service_v1" "common_platform_db_service" {
+  metadata {
+    name      = var.common_platform_db_external_name
+    namespace = var.env_name
+  }
+}
+
+data "kubernetes_service_v1" "common_mojaloop_db_service" {
+  metadata {
+    name      = var.common_mojaloop_db_external_name
+    namespace = var.env_name
+  }
+}
+
+
+resource "vault_kv_secret_v2" "common_platform_db_endpoint" {
+  mount               = var.kv_path
+  name                = "${var.env_name}/common_platform_db_endpoint"
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      value = try(data.kubernetes_service_v1.common_platform_db_service.spec.externalName, "")
+    }
+  )
+}
+
+
+resource "vault_kv_secret_v2" "common_mojaloop_db_endpoint" {
+  mount               = var.kv_path
+  name                = "${var.env_name}/common_platform_db_endpoint"
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      value = try(data.kubernetes_service_v1.common_mojaloop_db_service.spec.externalName, "")
+    }
+  )
+}
