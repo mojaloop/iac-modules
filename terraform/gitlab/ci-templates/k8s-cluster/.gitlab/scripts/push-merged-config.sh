7143952259660f219e88bc7df08839a1dec6e743
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-03-26 17:27:29 +0530

    push merged config

--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/push-merged-config.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/push-merged-config.sh
@@ -1,5 +1,7 @@
+BRANCH=$1
 git config --global user.email "root@${gitlab_hostname}"
 git config --global user.name "root"
+git checkout $BRANCH
 git add $CONFIG_PATH
 git diff --cached --exit-code || git commit -m "update merged configs"
 git push
