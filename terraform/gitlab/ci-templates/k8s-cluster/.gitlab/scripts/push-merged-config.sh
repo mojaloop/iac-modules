dbe58e857fbc00756cc82d63810855f1aee1f868
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-03-26 17:29:24 +0530

    changes

--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/push-merged-config.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/push-merged-config.sh
@@ -1,6 +1,10 @@
 BRANCH=$1
+ROOT_TOKEN=$2
+GIT_HOST=$3
+CURRENT_PROJECT_FULL_PATH=$4
 git config --global user.email "root@${gitlab_hostname}"
 git config --global user.name "root"
+git remote set-url origin https://root:${ROOT_TOKEN}@${GIT_HOST}/$CURRENT_PROJECT_FULL_PATH
 git checkout $BRANCH
 git add $CONFIG_PATH
 git diff --cached --exit-code || git commit -m "update merged configs"
