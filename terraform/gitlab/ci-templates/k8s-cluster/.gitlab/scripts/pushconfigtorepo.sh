92a96fd7ea4fdecea8629927c10951d4bfb9754c
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2025-05-15 14:04:22 +0100

    pushconfig: add trap, logging, and prevent empty commits

--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/pushconfigtorepo.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/pushconfigtorepo.sh
@@ -6,20 +6,27 @@ BRANCH=$3
 ROOT_TOKEN=$4
 GIT_CREDENTIALS=$5
 
-ENV_NAME=$(yq eval '.env' custom-config/cluster-config.yaml)
-DOMAIN=$(yq eval '.domain' custom-config/cluster-config.yaml)
-echo "ENV NAME = ${ENV_NAME}"
-echo "DOMAIN = ${DOMAIN}"
-
 WORKING_DIR=$PWD
 TMP_GITLAB_DIR=/tmp/gitlab_repo
 TMP_GITHUB_DIR=/tmp/github_repo
+
+cleanup() {
+    log "Cleaning up temporary directories..."
+    rm -rf "$TMP_GITLAB_DIR" "$TMP_GITHUB_DIR"
+}
+
+trap cleanup EXIT
+
+ENV_NAME=$(yq eval '.env' custom-config/cluster-config.yaml)
+DOMAIN=$(yq eval '.domain' custom-config/cluster-config.yaml)
+log "ENV NAME = ${ENV_NAME}"
+log "DOMAIN   = ${DOMAIN}"
 
 git clone https://root:${ROOT_TOKEN}@${GIT_HOST}/$CURRENT_PROJECT_FULL_PATH $TMP_GITLAB_DIR
 cd $TMP_GITLAB_DIR
 git checkout $BRANCH
 
-git clone -b feature/automate-config-files ${GIT_CREDENTIALS}/infitx-org/environments-custom-config.git $TMP_GITHUB_DIR
+git clone ${GIT_CREDENTIALS}/infitx-org/environments-custom-config.git $TMP_GITHUB_DIR
 
 cd $TMP_GITHUB_DIR
 python3 -m src.main $ENV_NAME $DOMAIN
@@ -30,8 +37,12 @@ cd $TMP_GITLAB_DIR
 git config --global user.email "root@${GIT_HOST}"
 git config --global user.name "root"
 git add custom-config/.
-git diff --cached --exit-code || git commit -m "Add custom config files"
+if ! git diff --cached --quiet; then
+    git commit -m "Add custom config files"
+    log "Pushing changes..."
+    git push
+else
+    log "No changes to commit."
+fi
 
-git push
-
-rm -rf $TMP_GITLAB_DIR $TMP_GITHUB_DIR
+log "Script completed successfully."
