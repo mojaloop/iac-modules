7e6af2900b5cbacb27b91ff74777296ce2d42d7b
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-07-07 12:33:01 +0530

    fix output

--- terraform/gitops/stateful-resources/outputs.tf
+++ terraform/gitops/stateful-resources/outputs.tf
@@ -5,7 +5,3 @@ output "stateful_resources" {
 output "managed_stateful_resources" {
   value = local.managed_stateful_resources
 }
-
-output "managed_resource_password_map" {
-  value = local.managed_resource_password_map
-}
