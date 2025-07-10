bc53685573c97375cb0f0152b678b2fe73487226
Sijo George <87609749+sijo5722-2021@users.noreply.github.com>
2025-07-10 11:18:37 +0530

    fix: duplicates

--- terraform/gitops/k8s-cluster-config/crossplane-providerconfig-config.tf
+++ terraform/gitops/k8s-cluster-config/crossplane-providerconfig-config.tf
@@ -1,20 +1,20 @@
-module "generate_crossplane_providers_files" {
+module "generate_crossplane_providerconfig_files" {
   source = "../generate-files"
   var_map = {
     crossplane_provider_config_sync_wave     = var.crossplane_provider_config_sync_wave
     gitlab_project_url                       = var.gitlab_project_url
     crossplane_namespace                     = var.crossplane_namespace
   }
-  file_list       = [for f in fileset(local.crossplane_providers_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.crossplane_providers_app_file, f))]
-  template_path   = local.crossplane_providers_template_path
+  file_list       = [for f in fileset(local.crossplane_providerconfig_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.crossplane_providerconfig_app_file, f))]
+  template_path   = local.crossplane_providerconfig_template_path
   output_path     = "${var.output_dir}/crossplane-provider-config"
-  app_file        = local.crossplane_providers_app_file
+  app_file        = local.crossplane_providerconfig_app_file
   app_output_path = "${var.output_dir}/app-yamls"
 }
 
 locals {
-  crossplane_providers_template_path    = "${path.module}/../generate-files/templates/crossplane-provider-config"
-  crossplane_providers_app_file         = "crossplane-providerconfig-app.yaml"
+  crossplane_providerconfig_template_path    = "${path.module}/../generate-files/templates/crossplane-provider-config"
+  crossplane_providerconfig_app_file         = "crossplane-providerconfig-app.yaml"
 }
 
 variable "crossplane_provider_config_sync_wave" {
