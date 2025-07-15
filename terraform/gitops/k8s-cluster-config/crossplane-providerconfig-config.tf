aedbd1921fb2050e98dafbafffbd1fe824636a07
Josphat Mutai <josphatkmutai@gmail.com>
2025-07-15 11:30:36 +0300

    fix cloud_provider

--- terraform/gitops/k8s-cluster-config/crossplane-providerconfig-config.tf
+++ terraform/gitops/k8s-cluster-config/crossplane-providerconfig-config.tf
@@ -4,6 +4,7 @@ module "generate_crossplane_providerconfig_files" {
     crossplane_provider_config_sync_wave     = var.crossplane_provider_config_sync_wave
     gitlab_project_url                       = var.gitlab_project_url
     crossplane_namespace                     = var.crossplane_namespace
+    cloud_provider                           = var.cloud_platform
   }
   file_list       = [for f in fileset(local.crossplane_providerconfig_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.crossplane_providerconfig_app_file, f))]
   template_path   = local.crossplane_providerconfig_template_path
