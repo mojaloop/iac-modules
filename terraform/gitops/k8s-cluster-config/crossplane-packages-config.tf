10181b76832893876f4d9070b8f17f407fffd233
Josphat Mutai <josphatkmutai@gmail.com>
2025-07-15 09:50:36 +0300

    fix: Add missing env vars

--- terraform/gitops/k8s-cluster-config/crossplane-packages-config.tf
+++ terraform/gitops/k8s-cluster-config/crossplane-packages-config.tf
@@ -10,6 +10,8 @@ module "generate_crossplane_packages_files" {
     crossplane_functions_extra_resources_version = var.crossplane_functions_extra_resources_version
     crossplane_packages_aws_documentdb_version = var.crossplane_packages_aws_documentdb_version
     crossplane_packages_aws_rds_version        = var.crossplane_packages_aws_rds_version
+    crossplane_packages_sc_mysql_version       = var.crossplane_packages_sc_mysql_version
+    crossplane_packages_sc_mongodb_version     = var.crossplane_packages_sc_mongodb_version
   }
   file_list       = [for f in fileset(local.crossplane_packages_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.crossplane_packages_app_file, f))]
   template_path   = local.crossplane_packages_template_path
@@ -36,6 +38,13 @@ variable "crossplane_packages_aws_documentdb_version" {
   type        = string
 }
 
+variable "crossplane_packages_sc_mysql_version" {
+  type        = string
+}
+
+variable "crossplane_packages_sc_mongodb_version" {
+  type        = string
+}
 
 variable "crossplane_packages_aws_rds_version" {
   type        = string
