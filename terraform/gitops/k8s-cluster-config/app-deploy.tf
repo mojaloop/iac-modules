5d94868c450811782b4455528002d06be12bdaa8
Josphat Mutai <josphatkmutai@gmail.com>
2025-07-15 11:18:11 +0300

    fixes private_dns_zone_id

--- terraform/gitops/k8s-cluster-config/app-deploy.tf
+++ terraform/gitops/k8s-cluster-config/app-deploy.tf
@@ -81,7 +81,7 @@ module "mojaloop" {
   database_subnets                     = var.database_subnets
   availability_zones                   = var.availability_zones
   cloud_region                         = var.cloud_region
-  zone_id                              = var.zone_id
+  private_dns_zone_id                  = var.private_dns_zone_id
 
 }
 
@@ -237,7 +237,7 @@ module "vnext" {
   database_subnets                     = var.database_subnets
   availability_zones                   = var.availability_zones
   cloud_region                         = var.cloud_region
-  zone_id                              = var.zone_id
+  private_dns_zone_id                  = var.private_dns_zone_id
 }
 
 variable "app_var_map" {
