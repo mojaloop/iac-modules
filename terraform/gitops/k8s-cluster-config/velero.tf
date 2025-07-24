b1f014a01750d5b3025f02cc9580af3ad464edd6
Radek Forgac <radek@forgac.io>
2025-07-24 13:41:52 +0200

    fix: object storage url

--- terraform/gitops/k8s-cluster-config/velero.tf
+++ terraform/gitops/k8s-cluster-config/velero.tf
@@ -6,7 +6,7 @@ module "generate_velero_files" {
     velero_namespace               = var.velero_namespace
     velero_backup_bucket_name       = local.velero_bucket
     object_store_velero_credentials_secret_name = "velero-credentials-secret"
-    object_store_api_url                           = var.object_store_api_url
+    object_store_api_url                           = "https://${var.object_store_api_url}"
     object_store_region            = var.object_store_region
     object_store_velero_secret_key = "cloud"
     velero_plugin_version          = var.velero_plugin_version
