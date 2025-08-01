1abb7bbab4f2776927f273d44b84f16a6b552639
Josphat Mutai <josphatkmutai@gmail.com>
2025-08-01 22:44:35 +0300

    fix: db_cluster_name

--- terraform/gitops/stateful-resources/stateful-resources-config.tf
+++ terraform/gitops/stateful-resources/stateful-resources-config.tf
@@ -186,7 +186,8 @@ resource "local_file" "dbaas-crs-mysql" {
   for_each = { for key, stateful_resource in local.monolith_env_mysql_dbaas_resources : key => stateful_resource }
   content = templatefile("${local.stateful_resources_template_path}/dbaas/${each.value.resource_type}/db-cluster.yaml.tpl",
     {
-        cluster_name                 = each.value.dbaas_resource_config.cluster_name
+        cluster_name                 = var.cluster_name
+        db_cluster_name              = each.value.dbaas_resource_config.cluster_name
         dbdeploy_name_prefix         = each.value.external_resource_config.dbdeploy_name_prefix
         namespace                    = each.value.resource_namespace
         appNamespace                 = each.value.resource_namespace
@@ -236,7 +237,8 @@ resource "local_file" "dbaas-crs-mongodb" {
   for_each = { for key, stateful_resource in local.monolith_env_mongo_dbaas_resources : key => stateful_resource }
   content = templatefile("${local.stateful_resources_template_path}/dbaas/${each.value.resource_type}/db-cluster.yaml.tpl",
     {
-        cluster_name                 = each.value.dbaas_resource_config.cluster_name
+        cluster_name                 = var.cluster_name
+        db_cluster_name              = each.value.dbaas_resource_config.cluster_name
         externalservice_name         = each.value.externalservice_name
         appNamespace                 = each.value.resource_namespace
         consumer_app_externalname_services = jsonencode(local.consumer_app_externalname_services[each.key])
