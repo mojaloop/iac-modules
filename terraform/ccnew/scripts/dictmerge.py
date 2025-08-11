597df34aa9314b8cd1abc49a653b4af22bf8a074
Radek Forgac <radek@forgac.io>
2025-08-11 19:04:30 +0200

    feat: edit dictmerge.py to use CC_VAR_

--- terraform/ccnew/scripts/dictmerge.py
+++ terraform/ccnew/scripts/dictmerge.py
@@ -19,38 +19,47 @@ def mergedicts(dict1, dict2):
             if isinstance(dict1[k], dict) and isinstance(dict2[k], dict):
                 yield (k, dict(mergedicts(dict1[k], dict2[k])))
             else:
-                # Value from custom dict overrides one in default 
-                #print("wrong one : k = " , k, " and dict2[k] = ", dict2[k], "and dict1[k] = ", dict1[k])
-                yield (k, dict2[k])
+                # Value from custom dict overrides one in default
+                # print("wrong one : k = " , k, " and dict2[k] = ", dict2[k], "and dict1[k] = ", dict1[k])
+                # check if env var is set
+                env_var = 'CC_VAR_' + k
+                if os.environ.get(env_var):
+                    yield (k, os.environ[env_var])
+                else:
+                    yield (k, dict2[k])
         elif k in dict1:
             yield (k, dict1[k])
         else:
-            yield (k, dict2[k])
+            env_var = 'CC_VAR_' + k
+            if os.environ.get(env_var):
+                yield (k, os.environ[env_var])
+            else:
+                yield (k, dict2[k])
 
 def writeDict(mergedItems, fileType, outputFilename):
     if fileType == ".json":
         with open(outputFilename, 'w') as file:
             json.dump(mergedItems, file, indent=4)
     elif fileType == ".yaml":
-        with open(outputFilename, 'w') as file:    
-            yaml.dump(mergedItems, file, indent=4,default_flow_style=False)    
+        with open(outputFilename, 'w') as file:
+            yaml.dump(mergedItems, file, indent=4,default_flow_style=False)
 
 
 def mergeListOfDicts(data1, data2, fileName, outputFilename, fileType):
     mergedItems=[]
-    
+
     if len(data2) == 0:
         print("The custom-config file ",fileName, "is empty ,so using the default configuration file")
         mergedItems = data1
         writeDict(mergedItems, fileType, outputFilename)
-        exit(0)     
-    
+        exit(0)
+
     if len(data1) != len(data2):
         print("The number of elements in custom-config and default_config differs for",fileName, ",so using the default configuration file")
         mergedItems = data1
         writeDict(mergedItems, fileType, outputFilename)
         exit(0)
-    
+
     for item1, item2 in zip(data1, data2):
         mergedItems.append(dict(mergedicts(item1, item2)))
     writeDict(mergedItems, fileType, outputFilename)
@@ -64,10 +73,10 @@ if len(sys.argv) == 4:
     defaultExt = os.path.splitext(default_config_file)[1]
     customExt = os.path.splitext(custom_config_file)[1]
     if defaultExt != customExt:
-        print("Please pass same type of files to merge")  
+        print("Please pass same type of files to merge")
         exit(1)
 else:
-    print("Please pass valid parameters usage : dictmerge.py defaultConfigFilePath customConfigFilePath outputPath")  
+    print("Please pass valid parameters usage : dictmerge.py defaultConfigFilePath customConfigFilePath outputPath")
     exit(1)
 
 if os.path.isfile(default_config_file):
@@ -76,10 +85,10 @@ if os.path.isfile(default_config_file):
             data1 = yaml.load(f, Loader=yaml.SafeLoader)
     elif defaultExt == ".json":
         with open(default_config_file, 'r') as f:
-            data1 = json.load(f)  
+            data1 = json.load(f)
     else:
        print("File type not supported")
-       exit(1)       
+       exit(1)
 else:
     print("Default config file "+default_config_file+" file does not exist")
     exit(1)
@@ -91,14 +100,14 @@ if os.path.isfile(custom_config_file):
     elif customExt == ".json":
         try:
             with open(custom_config_file, 'r') as f:
-                data2 = json.load(f)    
+                data2 = json.load(f)
         except json.JSONDecodeError:
             print("Could not parse the custom config file", custom_config_file," so assigning empty dict")
             data2 = {}
 
     else:
        print("File type not supported")
-       exit(1)                        
+       exit(1)
 else:
     print("Custom config file "+custom_config_file+" file does not exist. Assigning empty data dict")
     data2 = {}
