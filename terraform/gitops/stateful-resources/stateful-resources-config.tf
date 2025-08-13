52305d26d0667ef326eb0eca0d6d7868659ebb47
dfry <david@infitx.com>
2025-08-13 21:06:56 +0100

    rm unneeded var

--- terraform/gitops/stateful-resources/stateful-resources-config.tf
+++ terraform/gitops/stateful-resources/stateful-resources-config.tf
@@ -26,7 +26,6 @@ resource "local_file" "external_name_services" {
   content = templatefile("${local.stateful_resources_template_path}/external-name-services.yaml.tpl",
     { config                       = local.external_name_map
       stateful_resources_namespace = var.stateful_resources_namespace
-      service_entry_required       = var.create_service_entry
   })
   filename = "${local.stateful_resources_output_path}/external-name-services.yaml"
 }
