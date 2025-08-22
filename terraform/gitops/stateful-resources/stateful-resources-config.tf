eea693cfbdc3a211ec9074a42062ed6281a5849f
dfry <david@infitx.com>
2025-08-22 10:18:55 +0100

    fix merge issue

--- terraform/gitops/stateful-resources/stateful-resources-config.tf
+++ terraform/gitops/stateful-resources/stateful-resources-config.tf
@@ -22,12 +22,12 @@ resource "local_file" "vault_crs" {
 }
 
 
-resource "local_file" "non_managed_external_name_services" {
+resource "local_file" "external_name_services" {
   content = templatefile("${local.stateful_resources_template_path}/external-name-services.yaml.tpl",
     { config                       = local.external_name_map
       stateful_resources_namespace = var.stateful_resources_namespace
   })
-  filename = "${local.stateful_resources_output_path}/non-managed-external-name-services.yaml"
+  filename = "${local.stateful_resources_output_path}/external-name-services.yaml"
 }
 
 resource "local_file" "managed_service_entries" {
