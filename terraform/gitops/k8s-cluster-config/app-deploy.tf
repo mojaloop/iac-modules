6faec803f805e1b4ccd7d8aa6057760f5a2fb40c
Kalin Krustev <kalin.krustev@gmail.com>
2025-11-05 14:52:50 +0000

    feat: add namespace metadata to mojaloop module configuration

--- terraform/gitops/k8s-cluster-config/app-deploy.tf
+++ terraform/gitops/k8s-cluster-config/app-deploy.tf
@@ -88,6 +88,7 @@ module "mojaloop" {
   istio_nb_egress_waypoint_name                = var.istio_nb_egress_waypoint_name
   istio_nb_egress_waypoint_namespace           = var.istio_nb_egress_waypoint_namespace
   traces_endpoint                              = var.traces_endpoint
+  namespace_meta                               = local.namespace_meta
 }
 
 module "pm4ml" {
