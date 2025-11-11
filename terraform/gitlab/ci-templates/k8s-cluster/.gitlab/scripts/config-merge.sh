d755dd1a3868fac66596c3e0dc342fc438eb1d8f
Kalin Krustev <kalin.krustev@gmail.com>
2025-11-11 12:57:21 +0000

    fix: update config merge to consider addon folder names

--- terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/config-merge.sh
+++ terraform/gitlab/ci-templates/k8s-cluster/.gitlab/scripts/config-merge.sh
@@ -5,20 +5,18 @@ shopt -s nullglob globstar extglob
 ENV_TYPE=${ENV_TYPE:-dev}
 
 mkdir -p $CONFIG_PATH
-for configFile in $({ ls default-config/; ls custom-config/; } | sort -u)
+for configFile in $({ ls default-config/; ls custom-config/; find addons -mindepth 2 -maxdepth 2 -type d -printf '%f.yaml\n'; } | sort -u)
 do
     echo
     echo -n $configFile " ➡️ "
     ENV_CONFIG=${configFile/%.yaml/.$ENV_TYPE.yaml}
     ENV_CONFIG=${ENV_CONFIG/%.json/.$ENV_TYPE.json}
-    ADDON_CONFIG=${configFile/%.yaml/.config.yaml}
-    ADDON_CONFIG=${ADDON_CONFIG/%.json/.config.json}
     # merge configurations in the following order (later files override earlier ones):
     # default-config/*.(yaml|json)
-    # addons/**/*.config.(yaml|json)
-    # addons/**/xxx-*.config.(yaml|json) sorted by name
-    # addons/**/*.<env>.(yaml|json)
-    # addons/**/xxx-*.<env>.(yaml|json) sorted by name
+    # addons/*/.config/*.(yaml|json)
+    # addons/*/.config/xxx-*.(yaml|json) sorted by name
+    # addons/*/.config/*.<env>.(yaml|json)
+    # addons/*/.config/xxx-*.<env>.(yaml|json) sorted by name
     # profiles/**/*.(yaml|json)
     # profiles/**/xxx-*.(yaml|json) sorted by name
     # profiles/**/*.<env>.(yaml|json)
@@ -27,10 +25,10 @@ do
     # custom-config/xxx-*.(yaml|json) sorted by name
     python3 .gitlab/scripts/dictmerge.py \
         default-config/$configFile \
-        addons/**/@($ADDON_CONFIG) \
-        addons/**/+(*-)@($ADDON_CONFIG) \
-        addons/**/@($ENV_CONFIG) \
-        addons/**/+(*-)@($ENV_CONFIG) \
+        addons/*/.config/$configFile \
+        addons/*/.config/+(*-)$configFile \
+        addons/*/.config/$ENV_CONFIG \
+        addons/*/.config/+(*-)$ENV_CONFIG \
         profiles/**/$configFile \
         profiles/**/+(*-)$configFile \
         profiles/**/@($ENV_CONFIG) \
