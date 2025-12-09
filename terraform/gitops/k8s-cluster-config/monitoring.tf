a9437f8a7fa6c7167477003c55af1fca1a2f200e
Muzammil <muzammil360@gmail.com>
2025-12-09 16:54:48 +0500

    Migrate bitnami based grafana operator to official (#847)

--- terraform/gitops/k8s-cluster-config/monitoring.tf
+++ terraform/gitops/k8s-cluster-config/monitoring.tf
@@ -155,7 +155,7 @@ variable "metrics_server_replicas" {
 
 locals {
   bitnami_repo                        = "oci://registry-1.docker.io/bitnamicharts"
-  grafana_crd_version_tag             = "v5.6.0"
+  grafana_crd_version_tag             = "v5.20.0"
   prometheus_crd_version              = "8.0.1"
   opentelemetry_chart_version         = "0.93.1"
   grafana_wildcard_gateway            = var.grafana_ingress_internal_lb ? "internal" : "external"
@@ -169,7 +169,7 @@ locals {
   grafana_version                     = "11.6.1"
   grafana_dashboard_tag               = "v16.3.0-snapshot.17"     # NOTE: only for those dashboards which are in mojaloop/helm repo
   grafana_dashboard_tag_iac_modules   = "cto-1448--fix-cpu-throttling-alert" # tag for dashboards in mojaloop/iac-modules repo
-  grafana_operator_version            = "3.5.11"
+  grafana_operator_version            = "v5.20.0"
   monitoring_template_path            = "${path.module}/../generate-files/templates/monitoring"
   monitoring_app_file                 = "monitoring-app.yaml"
   loki_ingester_pvc_size              = "10Gi"
