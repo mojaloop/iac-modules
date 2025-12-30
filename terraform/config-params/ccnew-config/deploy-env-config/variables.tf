a71ed0772c619ef4b8aedfb41ca595f49ef6c693
dfry <david@infitx.com>
2025-12-30 08:39:11 +0000

    fix vars

--- terraform/config-params/ccnew-config/deploy-env-config/variables.tf
+++ terraform/config-params/ccnew-config/deploy-env-config/variables.tf
@@ -175,13 +175,13 @@ variable "oci_helm_repo_base_url" {
 locals {
   helm_classic_proxy_repos_string = join(",", [
     for item in split(",", var.helm_classic_proxy_repos) : 
-    "${split("=", item)[0]}=${var.classic_helm_repo_base_url}/${split("=", item)[0]}"
-    if length(trim(item)) > 0
+    "${split("=", item)[1]}=${var.classic_helm_repo_base_url}/${split("=", item)[0]}"
+    if length(trim(item, " ")) > 0
   ])
   
   helm_oci_proxy_repos_string = join(",", [
     for item in split(",", var.helm_oci_proxy_repos) : 
-    "${split("=", item)[0]}=${var.oci_helm_repo_base_url}/${split("=", item)[0]}"
-    if length(trim(item)) > 0
+    "${split("=", item)[1]}=${var.oci_helm_repo_base_url}/${split("=", item)[0]}"
+    if length(trim(item, " ")) > 0
   ])
 }
