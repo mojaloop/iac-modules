ee99bf399309690f8fdf8d0f931a249250f23af5
dfry <david@infitx.com>
2026-01-07 22:14:49 +0000

    debug helm_stateful_resources_resolved

--- terraform/gitops/stateful-resources/outputs.tf
+++ terraform/gitops/stateful-resources/outputs.tf
@@ -5,3 +5,7 @@ output "stateful_resources" {
 output "managed_stateful_resources" {
   value = local.managed_stateful_resources
 }
+
+output "helm_stateful_resources_resolved" {
+  value = local.helm_stateful_resources_resolved
+}
