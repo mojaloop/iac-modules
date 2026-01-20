89d32919b491889af92bc226b25f952b514b7503
Yevhen Kyriukha <kirgene@gmail.com>
2026-01-20 19:44:06 +0200

    wip

--- terraform/aws/base-k8s/outputs.tf
+++ terraform/aws/base-k8s/outputs.tf
@@ -11,6 +11,10 @@ output "external_load_balancer_dns" {
   value = aws_lb.lb.dns_name
 }
 
+output "external_load_balancer_private_ip" {
+  value = aws_lb.internal.dns_name
+}
+
 output "private_subdomain" {
   value = module.base_infra.public_int_zone.name
 }
