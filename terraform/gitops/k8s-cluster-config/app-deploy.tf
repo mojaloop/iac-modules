511d71fb7e6324ce69efcbae47fbd6842f8d8f1f
Vijay <vijaya.guthi@infitx.com>
2026-01-21 17:28:09 +0530

    fix: issues

--- terraform/gitops/k8s-cluster-config/app-deploy.tf
+++ terraform/gitops/k8s-cluster-config/app-deploy.tf
@@ -250,6 +250,9 @@ module "vnext" {
   oathkeeper_auth_provider_name        = local.oathkeeper_auth_provider_name
   keycloak_hubop_realm_name            = var.keycloak_hubop_realm_name
   mcm_admin_client_secret_name         = var.mcm_admin_client_secret_name
+  mcm_oidc_client_secret_secret        = var.mcm_oidc_client_secret_secret
+  mcm_oidc_client_secret_secret_key    = var.mcm_oidc_client_secret_secret_key
+  hubop_oidc_client_id                 = var.hubop_oidc_client_id
   smtp_from                            = var.app_var_map.smtp_from
   smtp_from_display_name               = var.app_var_map.smtp_from_display_name
   smtp_reply_to                        = var.app_var_map.smtp_reply_to
