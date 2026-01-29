fe4b14afd3658e46eab14cccd0f62f8f30fb76df
Yevhen Kyriukha <kirgene@gmail.com>
2026-01-30 01:00:01 +0200

    fix: add missing hubop_oidc variable declarations
    Add variable declarations for hubop_oidc_client_id and
    hubop_oidc_client_secret_secret that were missing in mojaloop module.

--- terraform/gitops/mojaloop/mojaloop.tf
+++ terraform/gitops/mojaloop/mojaloop.tf
@@ -502,6 +502,18 @@ variable "dfsp_oidc_client_secret_secret" {
   default     = "dfsp-oidc-client-secret"
 }
 
+variable "hubop_oidc_client_id" {
+  type        = string
+  description = "OIDC client ID for hub operator portal"
+  default     = "hubop-portal"
+}
+
+variable "hubop_oidc_client_secret_secret" {
+  type        = string
+  description = "Kubernetes secret name containing hub operator OIDC client secret"
+  default     = "hubop-oidc-client-secret"
+}
+
 variable "mcm_admin_client_secret_name" {
   type        = string
   description = "name of MCM admin client secret for Keycloak administrative operations"
