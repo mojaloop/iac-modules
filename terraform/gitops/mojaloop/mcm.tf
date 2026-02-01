b407ff92bcd7a00b581331c93989d973dee3c514
Yevhen Kyriukha <kirgene@gmail.com>
2026-02-01 04:17:05 +0200

    feat(mcm): add TTK MCM RBAC test configuration

--- terraform/gitops/mojaloop/mcm.tf
+++ terraform/gitops/mojaloop/mcm.tf
@@ -104,6 +104,8 @@ module "generate_mcm_files" {
     bulk_enabled                         = var.bulk_enabled
     ml_testing_toolkit_cli_chart_version = var.ml_testing_toolkit_cli_chart_version
     ttk_testcases_tag                    = try(var.app_var_map.ttk_testcases_tag, "")
+    ttk_mcm_rbac_testcases_tag           = try(var.app_var_map.ttk_mcm_rbac_testcases_tag, "20.3.0-mcm-rbac.0")
+    ttk_mcm_scripts_version              = try(var.app_var_map.ttk_mcm_scripts_version, "v3.7.0-snapshot.0")
     mailpit_namespace                    = var.mailpit_namespace
     smtp_from                            = var.smtp_from
     smtp_from_display_name               = var.smtp_from_display_name
