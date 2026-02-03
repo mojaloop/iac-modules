2aa08d340474188d2be39e0b89b0ce5b3cfd2131
Yevhen Kyriukha <kirgene@gmail.com>
2026-02-03 11:26:37 +0200

    chore: bump mcm_api_image_tag to v3.8.0-snapshot.17

--- terraform/gitops/mojaloop/mcm.tf
+++ terraform/gitops/mojaloop/mcm.tf
@@ -106,7 +106,7 @@ module "generate_mcm_files" {
     ml_testing_toolkit_cli_chart_version = var.ml_testing_toolkit_cli_chart_version
     ttk_testcases_tag                    = try(var.app_var_map.ttk_testcases_tag, "")
     ttk_mcm_rbac_testcases_tag           = try(var.app_var_map.ttk_mcm_rbac_testcases_tag, "20.3.0-mcm-rbac.2")
-    mcm_api_image_tag                    = try(var.app_var_map.mcm_api_image_tag, "v3.8.0-snapshot.16")
+    mcm_api_image_tag                    = try(var.app_var_map.mcm_api_image_tag, "v3.8.0-snapshot.17")
     portal_admin_secret                  = var.portal_admin_secret
     mailpit_namespace                    = var.mailpit_namespace
     smtp_from                            = var.smtp_from
