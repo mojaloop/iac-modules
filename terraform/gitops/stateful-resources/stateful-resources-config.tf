bf21727846be346e764e3f975e996190b3cf8f7a
muzammil360 <muzammil360@gmail.com>
2026-02-12 09:04:35 +0000

    switched to admin user

--- terraform/gitops/stateful-resources/stateful-resources-config.tf
+++ terraform/gitops/stateful-resources/stateful-resources-config.tf
@@ -397,8 +397,8 @@ resource "local_file" "monolith-mongodb-monitoring" {
         namespace                    = each.value.resource_namespace
         externalservice_name         = each.value.externalservice_name
         db_secret                    = each.value.external_resource_config.master_user_password_secret
-        db_secret_key                = "MONGODB_CLUSTER_MONITOR_PASSWORD" 
-        db_username_key              = "MONGODB_CLUSTER_MONITOR_USER"
+        db_secret_key                = "MONGODB_CLUSTER_ADMIN_PASSWORD" 
+        db_username_key              = "MONGODB_CLUSTER_ADMIN_USER"
         port                         = each.value.external_resource_config.port
         db_username                  = each.value.external_resource_config.username
         db_name                      = each.value.external_resource_config.db_name
