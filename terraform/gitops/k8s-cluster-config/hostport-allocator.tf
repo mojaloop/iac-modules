3f7079880731ed67e73236d44f300e47b73bca33
dfry <david@infitx.com>
2026-02-21 17:48:35 +0000

    fix hostport allocator

--- terraform/gitops/k8s-cluster-config/hostport-allocator.tf
+++ terraform/gitops/k8s-cluster-config/hostport-allocator.tf
@@ -1,20 +1,20 @@
-module "generate_certman_files" {
+module "generate_hostport_allocator_files" {
   source = "../generate-files"
   var_map = {
     hostport_allocator_version = var.hostport_allocator_version
     gitlab_project_url = var.gitlab_project_url
     hostport_allocator_sync_wave = var.hostport_allocator_sync_wave
   }
-  file_list       = [for f in fileset(local.certman_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.certman_app_file, f))]
-  template_path   = local.certman_template_path
-  output_path     = "${var.output_dir}/certmanager"
-  app_file        = local.certman_app_file
+  file_list       = [for f in fileset(local.hostport_allocator_template_path, "**/*.tpl") : trimsuffix(f, ".tpl") if !can(regex(local.hostport_allocator_app_file, f))]
+  template_path   = local.hostport_allocator_template_path
+  output_path     = "${var.output_dir}/hostport-allocator"
+  app_file        = local.hostport_allocator_app_file
   app_output_path = "${var.output_dir}/app-yamls"
 }
 
 locals {
-  certman_template_path = "${path.module}/../generate-files/templates/hostport-allocator"
-  certman_app_file      = "hostport-allocator-app.yaml"
+  hostport_allocator_template_path = "${path.module}/../generate-files/templates/hostport-allocator"
+  hostport_allocator_app_file      = "hostport-allocator-app.yaml"
 }
 
 variable "hostport_allocator_version" {
