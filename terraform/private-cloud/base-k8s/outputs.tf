23aa88dea135e726a4928f739598d268b78b4ab3
dfry <david@infitx.com>
2026-02-21 17:31:12 +0000

    output dummy var for private cloud

--- terraform/private-cloud/base-k8s/outputs.tf
+++ terraform/private-cloud/base-k8s/outputs.tf
@@ -213,6 +213,10 @@ output "availability_zones" {
   value = ["empty"]
 }
 
+output "target_group_vpn_health_port" {
+  value = var.target_group_vpn_health_port
+}
+
 locals {
 
   secrets_var_map = merge(var.app_var_map.create_iam_user ? {
