73e09fb4c0f824af48c50726a93229dc5bf951c0
muzammil360 <muzammil360@gmail.com>
2026-03-02 14:50:12 +0500

    remove unused loki retention configuration

--- terraform/gitops/k8s-cluster-config/monitoring.tf
+++ terraform/gitops/k8s-cluster-config/monitoring.tf
@@ -44,7 +44,6 @@ module "generate_monitoring_files" {
     istio_create_ingress_gateways              = var.istio_create_ingress_gateways
     loki_ingester_pvc_size                     = try(var.common_var_map.loki_ingester_pvc_size, local.loki_ingester_pvc_size)
     prometheus_pvc_size                        = try(var.common_var_map.prometheus_pvc_size, local.prometheus_pvc_size)
-    loki_retention_enabled                     = try(var.common_var_map.loki_retention_enabled, local.loki_retention_enabled)
     loki_ingester_retention_period             = try(var.common_var_map.loki_ingester_retention_period, local.loki_ingester_retention_period)
     loki_ingester_max_chunk_age                = try(var.common_var_map.loki_ingester_max_chunk_age, local.loki_ingester_max_chunk_age)
     loki_ingester_replication_factor           = try(var.common_var_map.loki_ingester_replication_factor, local.loki_ingester_replication_factor)
@@ -250,7 +249,6 @@ locals {
   monitoring_app_file                 = "monitoring-app.yaml"
   loki_ingester_pvc_size              = "10Gi"
   prometheus_pvc_size                 = "50Gi"
-  loki_retention_enabled              = true
   loki_ingester_retention_period      = "72h"
   loki_ingester_max_chunk_age         = "2h"
   loki_ingester_replication_factor    = "3"
